6a2a2c487d65717ad312df5b70789042b9df93cb
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-12-27 22:30:25 +0000

    Added premake file for the App/Game

---
+++ App/Build-App.lua
@@ -0,0 +1,48 @@
+project "App"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	targetdir "Binaries/%{cfg.buildcfg}"
+	staticruntime "off"
+
+	files { "include/**.h", "src/**.cpp" }
+
+	includedirs
+	{
+		-- Include Core
+		"include",
+		"../Core/2DGameEngine/include",
+		-- There are a lot of useful sdl structs/classes that the app could use
+		"../Core/SDL/SDL2-2.30.10/include",
+		"../Core/SDL/SDL2_image-2.8.2/include",
+		"../Core/SDL/SDL2_ttf-2.22.0/include",
+		"../Core/SDL/SDL2_mixer-2.8.0/include"
+	}
+
+	links
+	{
+		"../Core/Binaries/" .. OutputDir .. "/2DGameEngine/2DGameEngine.lib"
+	}
+
+	-- Ensure the app depends on the 2DGameEngine project
+	dependson { "2DGameEngine" }
+	
+	targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+	objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "RELEASE" }
+		runtime "Release"
+		optimize "On"
+		symbols "On"
+
+	filter "platforms:x86"
+		architecture "x86"
+
+	filter "platforms:x64"
+		architecture "x64"
