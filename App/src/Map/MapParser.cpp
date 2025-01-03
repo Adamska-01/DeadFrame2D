61f465190b9d4c5fcd262f08718b4d8d9245ce85
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-01-03 12:53:22 +0000

    Added 'MapParser' implementation

---
+++ App/src/Map/MapParser.cpp
@@ -0,0 +1,109 @@
+#include "Map/GameMap.h"
+#include "Map/MapParser.h"
+#include <sstream>
+#include <tinyxml2.h>
+
+
+using namespace tinyxml2;
+
+
+TileSet MapParser::ParseTileSet(XMLElement* xmlTileset)
+{
+	auto name = xmlTileset->Attribute("name");
+
+	auto firstID = xmlTileset->IntAttribute("firstgid");
+
+	auto tileCount = xmlTileset->IntAttribute("tilecount");
+	
+	// Allow using more than 1 tileset
+	auto lastID = (firstID + tileCount) - 1;
+
+	auto culumnCount = xmlTileset->IntAttribute("columns");
+	
+	auto rowCount = tileCount / culumnCount;
+
+	auto tileSize = xmlTileset->IntAttribute("tilewidth");
+
+	auto source = xmlTileset->FirstChildElement()->Attribute("source");
+
+	return TileSet(firstID, lastID, rowCount, culumnCount, tileCount, tileSize, name, source);
+}
+
+std::shared_ptr<TileLayer> MapParser::ParseTileLayer(XMLElement* xmlLayer, TileSetList tilesets, int tilesize, int rowcount, int colcount)
+{
+	// Get data (actual map)
+	XMLElement* data{ nullptr };
+	for (auto i = xmlLayer->FirstChildElement(); i != nullptr; i = i->NextSiblingElement())
+	{
+		if (i->Value() == "data")
+		{
+			data = i;
+			break;
+		}
+	}
+
+	// Parse through tile map
+	std::string matrix(data->GetText());
+	std::istringstream iss(matrix);
+	std::string id;
+
+	TileMap tilemap(rowcount, std::vector<int>(colcount, 0));
+	for (auto row = 0; row < rowcount; row++)
+	{
+		for (auto col = 0; col < colcount; col++)
+		{
+			// Discards ','
+			std::getline(iss, id, ','); 
+
+			std::stringstream convertor(id);
+			convertor >> tilemap[row][col];
+
+			if (!iss.good())
+				break;
+		}
+	}
+	
+	iss.clear();
+
+	return std::shared_ptr<TileLayer>(new TileLayer(tilesize, rowcount, colcount, tilemap, tilesets));
+}
+
+std::vector<std::shared_ptr<TileLayer>> MapParser::Parse(std::string source)
+{
+	XMLDocument xml;
+	
+	if (xml.LoadFile(source.c_str()) != XMLError::XML_SUCCESS)
+	{
+		std::cerr << "Failed to load: " << source << std::endl;
+
+		return {};
+	}
+
+	auto root = xml.RootElement();
+	
+	auto colCount = root->IntAttribute("width", 0);
+	auto rowCount = root->IntAttribute("height", 0);
+	auto tileSize = root->IntAttribute("tilewidth", 0);
+
+	// Parse tilesets
+	TileSetList tilesets;
+	for (auto i = root->FirstChildElement(); i != nullptr; i = i->NextSiblingElement())
+	{
+		if (i->Value() == "tileset")
+		{
+			tilesets.push_back(ParseTileSet(i));
+		}
+	}
+
+	// Parse Layers
+	std::vector<std::shared_ptr<TileLayer>> layers = {};
+	for (auto i = root->FirstChildElement(); i != nullptr; i = i->NextSiblingElement())
+	{
+		if (i->Value() == std::string("layer"))
+		{
+			layers.push_back(ParseTileLayer(i, tilesets, tileSize, rowCount, colCount));
+		}
+	}
+
+	return layers;
+}
