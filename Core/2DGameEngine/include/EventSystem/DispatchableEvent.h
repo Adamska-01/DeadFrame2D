fe406bb3a38c6f176f6ff2c4b63bef06ca00fc12
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-01-11 17:58:15 +0000

    Added base class for all event types

---
+++ Core/2DGameEngine/include/EventSystem/DispatchableEvent.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+class DispatchableEvent
+{
+public:
+	virtual ~DispatchableEvent() = default;
+
+
+	template <typename T>
+	static std::shared_ptr<T> SafeCast(std::shared_ptr<DispatchableEvent> event) 
+	{
+		auto castedEvent = std::dynamic_pointer_cast<T>(event);
+		
+		if (castedEvent == nullptr) 
+		{
+			std::cerr << "Event received is not a valid type of " << typeid(T).name() << std::endl;
+		}
+
+		return castedEvent;
+	}
+};
