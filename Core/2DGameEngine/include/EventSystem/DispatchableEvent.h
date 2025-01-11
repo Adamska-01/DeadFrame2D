46b4f55a0261f52f3717289d9046d9d726932f80
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-01-11 18:00:46 +0000

    Moved template function outside the 'DispatchableEvent' class

--- Core/2DGameEngine/include/EventSystem/DispatchableEvent.h
+++ Core/2DGameEngine/include/EventSystem/DispatchableEvent.h
@@ -8,15 +8,19 @@ public:
 
 
 	template <typename T>
-	static std::shared_ptr<T> SafeCast(std::shared_ptr<DispatchableEvent> event) 
-	{
-		auto castedEvent = std::dynamic_pointer_cast<T>(event);
-		
-		if (castedEvent == nullptr) 
-		{
-			std::cerr << "Event received is not a valid type of " << typeid(T).name() << std::endl;
-		}
-
-		return castedEvent;
-	}
+	static std::shared_ptr<T> SafeCast(std::shared_ptr<DispatchableEvent> event);
 };
+
+
+template<typename T>
+inline std::shared_ptr<T> DispatchableEvent::SafeCast(std::shared_ptr<DispatchableEvent> event)
+{
+	auto castedEvent = std::dynamic_pointer_cast<T>(event);
+
+	if (castedEvent == nullptr)
+	{
+		std::cerr << "Event received is not a valid type of " << typeid(T).name() << std::endl;
+	}
+
+	return castedEvent;
+}
