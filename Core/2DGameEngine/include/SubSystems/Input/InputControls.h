f54aa1d8fa6d3381fcf0ef67ee78c9f1ac321ab6
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-01-24 21:09:42 +0000

    Added a first draft of the 'InputControls' class

---
+++ Core/2DGameEngine/include/SubSystems/Input/InputControls.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Generic/InputBinding.h"
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+
+class InputControls
+{
+private:
+	static std::unordered_map<std::string, std::vector<InputBinding>> inputActions;
+
+
+public:
+	static void AddOrOverrideAction(const std::string& actionName, const std::vector<InputBinding>& bindings);
+
+	static bool RemoveAction(const std::string& actionName);
+
+	static bool AddBinding(const std::string& actionName, const InputBinding& binding);
+
+	static bool RemoveBinding(const std::string& actionName, const InputBinding& binding);
+
+	static std::optional<std::vector<InputBinding>> GetAction(const std::string& actionName);
+
+	static void PrintActions();
+};
---
+++ Core/2DGameEngine/src/SubSystems/Input/InputControls.cpp
@@ -0,0 +1,69 @@
+#include "SubSystems/Input/InputControls.h"
+#include <iostream>
+
+
+void InputControls::AddOrOverrideAction(const std::string& actionName, const std::vector<InputBinding>& bindings)
+{
+	inputActions[actionName] = bindings;
+}
+
+bool InputControls::RemoveAction(const std::string& actionName)
+{
+	return inputActions.erase(actionName);
+}
+
+bool InputControls::AddBinding(const std::string& actionName, const InputBinding& binding)
+{
+	auto it = inputActions.find(actionName);
+
+	if (it == inputActions.end())
+		return false;
+
+	it->second.push_back(binding);
+
+	return false;
+}
+
+bool InputControls::RemoveBinding(const std::string& actionName, const InputBinding& binding)
+{
+	auto it = inputActions.find(actionName);
+
+	if (it == inputActions.end())
+		return false;
+
+	auto& bindings = it->second;
+	auto bindingIt = std::find_if(bindings.begin(), bindings.end(), [&](const InputBinding& b)
+		{
+			return b.inputDeviceType == binding.inputDeviceType && b.inputKey == binding.inputKey;
+		});
+
+	if (bindingIt == bindings.end())
+		return false;
+
+	bindings.erase(bindingIt);
+
+	return true;
+}
+
+std::optional<std::vector<InputBinding>> InputControls::GetAction(const std::string& actionName)
+{
+	auto it = inputActions.find(actionName);
+
+	if (it != inputActions.end())
+		return it->second;
+	
+	return std::nullopt;
+}
+
+void InputControls::PrintActions()
+{
+	for (const auto& [actionName, bindings] : inputActions)
+	{
+		std::cout << "Action: " << actionName << "\n";
+		for (const auto& binding : bindings)
+		{
+			std::cout << "  - Device: " << (binding.inputDeviceType == InputDeviceType::KEYBOARD ? "Keyboard" : "Controller")
+				<< ", Key: " << static_cast<int>(binding.inputKey) << "\n";
+		}
+	}
+}
