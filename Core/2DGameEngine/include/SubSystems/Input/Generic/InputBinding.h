73ab003340c8fc74b1ab9192f16a6e1da06d3715
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-01-25 18:44:34 +0000

    Added serialization support in 'InputBinding'

--- Core/2DGameEngine/include/SubSystems/Input/Generic/InputBinding.h
+++ Core/2DGameEngine/include/SubSystems/Input/Generic/InputBinding.h
@@ -9,3 +9,20 @@ struct InputBinding
 
 	Uint8 inputKey;
 };
+
+
+// Serialization functions (nlohmann)
+inline void to_json(nlohmann::json& j, const InputBinding& binding)
+{
+	j = nlohmann::json
+	{
+		{"inputDeviceType", binding.inputDeviceType},
+		{"inputKey", binding.inputKey}
+	};
+}
+
+inline void from_json(const nlohmann::json& j, InputBinding& binding) 
+{
+	j.at("inputDeviceType").get_to(binding.inputDeviceType);
+	j.at("inputKey").get_to(binding.inputKey);
+}
