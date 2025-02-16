b78fd7da0eb3e157ddb04a6545bb58e8cfc7d407
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-02-16 13:20:47 +0000

    Added a function to get a specific property to 'TiledLayer'

--- Core/2DGameEngine/include/TileEditors/Tiled/Models/TiledLayer.h
+++ Core/2DGameEngine/include/TileEditors/Tiled/Models/TiledLayer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "TiledProperty.h"
+#include <optional>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -10,4 +11,15 @@ struct TiledLayer
 	std::unordered_map<std::string, TiledProperty> Properties;
 
 	std::vector<std::vector<int>> Data;
+
+
+	std::optional<TiledProperty> GetProperty(const std::string& key) const 
+	{
+		auto it = Properties.find(key);
+		
+		if (it != Properties.end()) 
+			return it->second;
+
+		return std::nullopt; 
+	}
 };
