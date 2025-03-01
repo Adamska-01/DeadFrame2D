900771a233c2497c7d64db306171405afc38b352
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-03-01 16:16:20 +0000

    Added 'RayVsRect' utility function

--- Core/2DGameEngine/include/Tools/Collisions/CollisionHandler.h
+++ Core/2DGameEngine/include/Tools/Collisions/CollisionHandler.h
@@ -20,6 +20,8 @@ public:
 	static bool PointVsCircle(const Vector2F& point, const Circle* circle);
 
 	static bool RectVsRect(const SDL_Rect* boxA, const SDL_Rect* boxB);
+
+	static bool RayVsRect(const Vector2F& ray_origin, const Vector2F& ray_dir, const SDL_Rect* target, Vector2F& contact_point, Vector2F& contact_normal, float& t_hit_near);
 	
 	
 	virtual bool Visit(BoxCollider2D* box, BoxCollider2D* other) override;
--- Core/2DGameEngine/src/Tools/Collisions/CollisionHandler.cpp
+++ Core/2DGameEngine/src/Tools/Collisions/CollisionHandler.cpp
@@ -30,6 +30,64 @@ bool CollisionHandler::RectVsRect(const SDL_Rect* boxA, const SDL_Rect* boxB)
 		&& boxB->y < boxA->y + boxA->h;
 }
 
+bool CollisionHandler::RayVsRect(const Vector2F& ray_origin, const Vector2F& ray_dir, const SDL_Rect* target, Vector2F& contact_point, Vector2F& contact_normal, float& t_hit_near)
+{
+	contact_normal = { 0,0 };
+	contact_point = { 0,0 };
+
+	// Cache division
+	auto invdir = Vector2F(1.0f / ray_dir.x, 1.0f / ray_dir.y);
+
+	// Calculate intersections with rectangle bounding axes
+	auto t_near = (Vector2F(target->x, target->y) - ray_origin) * invdir;
+	auto t_far = (Vector2F(target->x, target->y) + Vector2F(target->w, target->h) - ray_origin) * invdir;
+
+	if (std::isnan(t_far.y) || std::isnan(t_far.x) || std::isnan(t_near.y) || std::isnan(t_near.x)) 
+		return false;
+
+	// Sort distances
+	if (t_near.x > t_far.x)
+	{
+		std::swap(t_near.x, t_far.x);
+	}
+	
+	if (t_near.y > t_far.y)
+	{
+		std::swap(t_near.y, t_far.y);
+	}
+
+	// Early rejection
+	if (t_near.x > t_far.y || t_near.y > t_far.x) 
+		return false;
+
+	// Closest 'time' will be the first contact
+	t_hit_near = std::max(t_near.x, t_near.y);
+
+	// Furthest 'time' is contact on opposite side of target
+	auto t_hit_far = std::min(t_far.x, t_far.y);
+
+	// Reject if ray direction is pointing away from object
+	if (t_hit_far < 0)
+		return false;
+
+	// Contact point of collision from parametric line equation
+	contact_point = ray_origin + ray_dir * t_hit_near;
+
+	if (t_near.x > t_near.y)
+	{
+		contact_normal = invdir.x < 0 ? Vector2F(1, 0) : Vector2F(-1, 0);
+	}
+	else if (t_near.x < t_near.y)
+	{
+		contact_normal = invdir.y < 0 ? Vector2F(0, 1) : Vector2F(0, -1);
+	}
+
+	// Note if t_near == t_far, collision is principly in a diagonal
+	// so pointless to resolve. By returning a CN={0,0} even though its
+	// considered a hit, the resolver wont change anything.
+	return true;
+}
+
 bool CollisionHandler::Visit(BoxCollider2D* box, BoxCollider2D* other)
 {
 	auto A = box->GetCollisionBox();
