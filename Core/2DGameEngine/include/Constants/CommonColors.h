a8cd21b7065289c0cca5b5089660a229cf26c6c6
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-03-15 18:17:15 +0000

    Added CommonColors

---
+++ Core/2DGameEngine/include/Constants/CommonColors.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <SDL_pixels.h>
+
+
+namespace CommonColors
+{
+	constexpr SDL_Color WHITE = { 255, 255, 255, 255 };
+	
+	constexpr SDL_Color RED = { 255, 0, 0, 255 };
+	
+	constexpr SDL_Color GREEN = { 0, 255, 0, 255 };
+	
+	constexpr SDL_Color BLUE = { 0, 0, 255, 255 };
+	
+	constexpr SDL_Color YELLOW = { 255, 255, 0, 255 };
+	
+	constexpr SDL_Color CYAN = { 0, 255, 255, 255 };
+	
+	constexpr SDL_Color MAGENTA = { 255, 0, 255, 255 };
+	
+	constexpr SDL_Color BLACK = { 0, 0, 0, 255 };
+	
+	constexpr SDL_Color GRAY = { 169, 169, 169, 255 };
+	
+	constexpr SDL_Color DARK_GRAY = { 169, 169, 169, 255 };
+	
+	constexpr SDL_Color LIGHT_GRAY = { 211, 211, 211, 255 };
+	
+	constexpr SDL_Color ORANGE = { 255, 165, 0, 255 };
+	
+	constexpr SDL_Color PURPLE = { 128, 0, 128, 255 };
+	
+	constexpr SDL_Color BROWN = { 139, 69, 19, 255 };
+	
+	constexpr SDL_Color PINK = { 255, 192, 203, 255 };
+	
+	constexpr SDL_Color LIGHT_BLUE = { 173, 216, 230, 255 };
+	
+	constexpr SDL_Color LIME = { 0, 255, 0, 255 };
+	
+	constexpr SDL_Color INDIGO = { 75, 0, 130, 255 };
+	
+	constexpr SDL_Color TURQUOISE = { 64, 224, 208, 255 };
+	
+	constexpr SDL_Color TEAL = { 0, 128, 128, 255 };
+	
+	constexpr SDL_Color IVORY = { 255, 255, 240, 255 };
+	
+	constexpr SDL_Color SALMON = { 250, 128, 114, 255 };
+}
