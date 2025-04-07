970385dbf51fea0f0d69933dc4c19d1e62d5d072
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-07 16:54:47 +0100

    Added functions to 'GameMap'

--- App/include/Map/GameMap.h
+++ App/include/Map/GameMap.h
@@ -1,11 +1,29 @@
 #pragma once
 #include <GameObject.h>
-#include <TileEditors/Tiled/Parsers/TiledMapParser.h>
 #include <vector>
+
+
+struct TiledMap;
+struct TiledLayer;
+class TiledMapCompatibleRenderer;
+class TiledMapCompatibleCollider2D;
 
 
 class GameMap : public GameObject
 {
+private:
+	std::shared_ptr<TiledMap> fullTileMap;
+
+	TiledMapCompatibleRenderer* tileRenderer;
+
+	TiledMapCompatibleCollider2D* tileCollider;
+
+
+	std::shared_ptr<TiledMap> RetrieveRenderMap();
+
+	std::vector<TiledLayer> RetrieveCollisionMap();
+
+
 public:
 	GameMap(const char* mapSource, bool extendMapToRenderTarget = false);
 
--- App/src/Map/GameMap.cpp
+++ App/src/Map/GameMap.cpp
@@ -1,8 +1,62 @@
+#include "Constants/TiledPropertyNames.h"
 #include "Map/GameMap.h"
+#include <Components/Collisions/Tile/Tiled/TiledMapCompatibleCollider2D.h>
 #include <Components/TileMap/Tiled/TiledMapCompatibleRenderer.h>
+#include <TileEditors/Tiled/Parsers/TiledMapParser.h>
+#include <variant>
 
 
 GameMap::GameMap(const char* mapSource, bool extendMapToRenderTarget)
 {
-	AddComponent<TiledMapCompatibleRenderer>(mapSource, extendMapToRenderTarget);
+	fullTileMap = TiledMapParser().Parse(mapSource);
+
+	tileRenderer = AddComponent<TiledMapCompatibleRenderer>(RetrieveRenderMap(), extendMapToRenderTarget);
+	tileCollider = AddComponent<TiledMapCompatibleCollider2D>(RetrieveCollisionMap());
+}
+
+std::shared_ptr<TiledMap> GameMap::RetrieveRenderMap()
+{
+	if (fullTileMap == nullptr)
+		throw std::runtime_error("Trying to access tile map but Tile Renderer component is null...");
+
+	auto renderLayer = std::vector<TiledLayer>();
+
+	for (const auto& layer : fullTileMap->layers)
+	{
+		// TODO: Replace property name with a constant
+		auto renderEnabled = layer.GetProperty(TiledPropertyNames::RENDER_ENABLED);
+
+		if (!renderEnabled.has_value() || !std::get<bool>(renderEnabled.value().Value))
+			continue;
+
+		renderLayer.push_back(layer);
+	}
+
+	return std::make_shared<TiledMap>(
+		fullTileMap->width,
+		fullTileMap->height,
+		fullTileMap->tileSize,
+		fullTileMap->tileSets,
+		renderLayer);
+}
+
+std::vector<TiledLayer> GameMap::RetrieveCollisionMap()
+{
+	if (fullTileMap == nullptr)
+		throw std::runtime_error("Trying to access tile map but Tile Renderer component is null...");
+
+	auto colliderMap = std::vector<TiledLayer>();
+
+	for (const auto& layer : fullTileMap->layers)
+	{
+		// TODO: Replace property name with a constant
+		auto isSolid = layer.GetProperty(TiledPropertyNames::IS_SOLID);
+
+		if (!isSolid.has_value() || !std::get<bool>(isSolid.value().Value))
+			continue;
+
+		colliderMap.push_back(layer);
+	}
+
+	return colliderMap;
 }
