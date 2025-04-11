2967f869e96ff41a4986971a189dadd21f979f61
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-11 19:29:16 +0100

    Added an enum to reppresent game modes

---
+++ App/include/Generic/GameMode.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+/**
+ * @enum GameMode
+ * @brief Represents the different game modes available.
+ */
+enum class GameMode
+{
+	/**
+	 * @brief Single player mode.
+	 */
+	SINGLE_PLAYER,
+
+	/**
+	 * @brief Multiplayer mode.
+	 */
+	MULTIPLAYER
+};
