98d33ad27b7745cff8299ec8ac7e3d5bd0362f90
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-12 20:25:54 +0100

    Added PlayerInputConstants

---
+++ App/include/Constants/PlayerInputConstants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+namespace PlayerInputConstants
+{
+	inline constexpr float CANNON_MIN_ROTATION = -75.0f;
+
+	inline constexpr float CANNON_MAX_ROTATION = 75.0f;
+
+	inline constexpr float CANNON_ROTATION_SPEED = 100.0f;
+}
