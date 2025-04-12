3982e1ff026e327758233b1dd781e0cf4c364ac7
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-12 20:28:20 +0100

    Added AssetPaths constants

---
+++ App/include/Constants/AssetPaths.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string_view>
+
+
+namespace AssetPaths
+{
+	inline constexpr std::string_view SINGLEPLAYER_MAP_PATH = "App/Assets/Maps/SingleplayerMap.tmx";
+	
+	inline constexpr std::string_view MULTIPLAYER_MAP_PATH = "App/Assets/Maps/MultiplayerMap.tmx";
+
+	inline constexpr std::string_view ARROW_IMAGE_PATH = "App/Assets/Sprites/Arrow.png";
+}
