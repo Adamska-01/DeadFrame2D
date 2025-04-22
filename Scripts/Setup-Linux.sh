6a2ed534393bff14663b7d36e82e5f99af289acf
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-22 11:01:55 +0100

    Added post-checkout hook to regenerate project files

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+set -e
+
+# Config
+PREMAKE_PATH="Vendor/Premake/Linux/premake5"
+HOOK_PATH="../.git/hooks/post-checkout"
+
+
+# Run Premake
+echo Generating project files...
 pushd ..
-Vendor/Premake/Linux/premake5 --cc=clang --file=Build.lua gmake2
+$PREMAKE_PATH --cc=clang --file=Build.lua gmake2
 popd
+echo Project files generated!
+
+
+# Install Git post-checkout hook
+echo "Installing Git post-checkout hook..."
+cat > "$HOOK_PATH" << 'EOF'
+#!/bin/sh
+echo "Regenerating project files with Premake..."
+$PREMAKE_PATH --cc=clang --file=Build.lua gmake2
+EOF
+chmod +x "$HOOK_PATH"
+echo "Hook installed at $HOOK_PATH"
--- Scripts/Setup-Windows.bat
+++ Scripts/Setup-Windows.bat
@@ -1,6 +1,25 @@
 @echo off
 
+# Config
+set "PREMAKE_PATH=Vendor\Premake\Windows\premake5.exe"
+set "HOOK_PATH=..\.git\hooks\post-checkout"
+
+
+:: Run Premake
+echo Generating project files...
 pushd ..
-Vendor\Premake\Windows\premake5.exe --file=Build.lua vs2022
+%PREMAKE_PATH% --file=Build.lua vs2022
 popd
+echo Project files generated!
+
+
+:: Install Git hook (post-checkout)
+echo Installing Git post-checkout hook...
+(
+    echo #!/bin/sh
+    echo echo "Regenerating project files with Premake..."
+    echo %PREMAKE_PATH% --file=Build.lua vs2022
+) > %HOOK_PATH%
+echo Hook installed at %HOOK_PATH%
+
 pause
