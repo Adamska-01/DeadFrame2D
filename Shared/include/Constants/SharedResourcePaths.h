b40473524be4148b023c96b31380ec6f025588c3
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-26 19:19:05 +0100

    Added 'SharedResourcePaths' to the Shared project

---
+++ Shared/include/Constants/SharedResourcePaths.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string_view>
+
+
+namespace SharedResourcePaths
+{
+	namespace Fonts
+	{
+		inline constexpr std::string_view CONSOLAS_FONT_PATH = "Shared/Resources/Fonts/consola.ttf";
+	}
+}
