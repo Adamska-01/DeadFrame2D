65053fba6b87de3a82828fdc9338126dd8dce49e
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-26 19:57:21 +0100

    Sorted 'MainMenuScene'

--- App/src/Scenes/MainMenuScene.cpp
+++ App/src/Scenes/MainMenuScene.cpp
@@ -1,28 +1,35 @@
 #include "Scenes/GameScene.h"
 #include "Scenes/MainMenuScene.h"
+#include <Blueprints/UI/ButtonBlueprint.h>
 #include <Components/Transform.h>
-#include <Components/UI/Button.h>
+#include <Constants/AssetPaths.h>
+#include <Constants/SharedResourcePaths.h>
 #include <Management/SceneManager.h>
+#include <Models/Blueprints/UI/ButtonBlueprintModel.h>
 #include <SubSystems/Window.h>
 
 
 void MainMenuScene::Enter()
 {
-	auto a = GameObject::Instantiate<GameObject>();
-	a.lock()->AddComponent<Button>(
-		[]() { SceneManager::LoadScene<GameScene>(); },
-		"App/Assets/Sprites/Buttons/Button_Idle.png",
-		"App/Assets/Sprites/Buttons/Button_Hovered.png",
-		"App/Assets/Sprites/Buttons/Button_Pressed.png",
-		"Press To Crash",
-		Vector2F{ 300.0f, 100.0f }
-	);
-	auto dumb = GameObject::Instantiate<GameObject>();
-	auto dumb2 = GameObject::Instantiate<GameObject>();
-	dumb.lock()->AddChildGameObject(dumb2);
-	a.lock()->AddChildGameObject(dumb);
-	a.lock()->AddChildGameObject(dumb2);
+	auto buttonConfiguration = ButtonBlueprintModel
+	{
+		ButtonComponentModel
+		{
+			.onPressedHandler = []() { SceneManager::LoadScene<GameScene>(); },
+			.idleButtonSource = AssetPaths::Sprites::BUTTON_IDLE_IMAGE_PATH,
+			.hoveredButtonSource = AssetPaths::Sprites::BUTTON_HOVERED_IMAGE_PATH,
+			.pressedButtonSource = AssetPaths::Sprites::BUTTON_PRESSED_IMAGE_PATH,
+			.buttonSize = Vector2F{ 300.0f, 100.0f }
+		},
+		TextMeshComponentModel
+		{
+			.fontSource = SharedResourcePaths::Fonts::CONSOLAS_FONT_PATH,
+			.text = "Single Player",
+			.linesNumber = 1
+		}
+	};
 
-	auto components = a.lock()->GetComponentsInChildren<Transform>();
-	a.lock()->GetComponent<Transform>()->SetWorldPosition(Vector2F(Window::GetResolution().x * 0.5f, Window::GetResolution().y * 0.5f));
+	auto button = GameObject::Instantiate<ButtonBlueprint>(buttonConfiguration);
+
+	button.lock()->GetTransform()->SetWorldPosition(Vector2F(Window::GetResolution().x * 0.5f, Window::GetResolution().y * 0.5f));
 }
