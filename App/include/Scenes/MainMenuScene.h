b3d5765938b704c931063ffb2053fea3c2b95f69
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-28 20:19:38 +0100

    Added more stuff to the 'MainMenuScene'

--- App/include/Scenes/MainMenuScene.h
+++ App/include/Scenes/MainMenuScene.h
@@ -1,9 +1,24 @@
 #pragma once
 #include <Management/Scene.h>
+#include <Math/Vector2.h>
+#include <memory>
+
+
+class ButtonBlueprint;
 
 
 class MainMenuScene : public Scene
 {
+private:
+	void CreateBackground(const Vector2I& renderTargetSize);
+	
+	void CreateTitle(const Vector2I& renderTargetSize);
+	
+	void SetupLayoutGroup(const Vector2I& renderTargetSize);
+	
+	std::weak_ptr<ButtonBlueprint> CreateButton(const std::string& text, const std::function<void()>& onPressedHandler);
+
+
 public:
 	MainMenuScene() = default;
 
--- App/src/Scenes/MainMenuScene.cpp
+++ App/src/Scenes/MainMenuScene.cpp
@@ -1,35 +1,80 @@
 #include "Scenes/GameScene.h"
 #include "Scenes/MainMenuScene.h"
 #include <Blueprints/UI/ButtonBlueprint.h>
+#include <Components/Rendering/ImageScroller.h>
+#include <Components/Rendering/Sprite.h>
 #include <Components/Transform.h>
+#include <Components/UI/Layout/VerticalLayoutGroup.h>
 #include <Constants/AssetPaths.h>
-#include <Constants/SharedResourcePaths.h>
 #include <Management/SceneManager.h>
 #include <Models/Blueprints/UI/ButtonBlueprintModel.h>
-#include <SubSystems/Window.h>
+#include <SubSystems/Events/EventManager.h>
+#include <SubSystems/Renderer.h>
 
 
-void MainMenuScene::Enter()
+void MainMenuScene::CreateBackground(const Vector2I& renderTargetSize)
 {
-	auto buttonConfiguration = ButtonBlueprintModel
-	{
-		ButtonComponentModel
-		{
-			.onPressedHandler = []() { SceneManager::LoadScene<GameScene>(); },
+	auto backgroundObject = GameObject::Instantiate<GameObject>();
+	
+	backgroundObject.lock()->AddComponent<ImageScroller>(AssetPaths::Backgrounds::BACKGROUND_IMAGE_PATH, ScrollDirection::HORIZONTAL, 50.0f);
+	backgroundObject.lock()->GetTransform()->Scale(Vector2(0.5f, 0.5f));
+}
+
+void MainMenuScene::CreateTitle(const Vector2I& renderTargetSize)
+{
+	auto title = GameObject::Instantiate<GameObject>();
+	
+	title.lock()->AddComponent<Sprite>(AssetPaths::Sprites::TITLE_IMAGE_PATH);
+	title.lock()->GetTransform()->SetWorldPosition(Vector2F(renderTargetSize.x * 0.5f, renderTargetSize.y * 0.2f));
+	title.lock()->GetTransform()->Scale(Vector2(0.5f, 0.5f));
+}
+
+void MainMenuScene::SetupLayoutGroup(const Vector2I& renderTargetSize)
+{
+	auto layoutGroup = GameObject::Instantiate<GameObject>();
+
+	layoutGroup.lock()->AddComponent<VerticalLayoutGroup>(20.0f, LayoutPadding());
+	layoutGroup.lock()->GetComponent<Transform>()->SetWorldPosition(Vector2F(renderTargetSize.x * 0.5f, renderTargetSize.y * 0.65f));
+
+	auto button1 = CreateButton("SinglePlayer", []() { SceneManager::LoadScene<GameScene>(); });
+	auto button2 = CreateButton("Multiplayer", []() { /* Multiplayer logic */ });
+	//auto button3 = CreateButton("Settings", []() { /* Settings logic */ });
+	auto button4 = CreateButton("Exit", []() { EventManager::SendSystemEvent(SDL_EventType::SDL_QUIT); });
+
+	layoutGroup.lock()->AddChildGameObject(button1);
+	layoutGroup.lock()->AddChildGameObject(button2);
+	//layoutGroup.lock()->AddChildGameObject(buttonextra);
+	layoutGroup.lock()->AddChildGameObject(button4);
+}
+
+std::weak_ptr<ButtonBlueprint> MainMenuScene::CreateButton(const std::string& text, const std::function<void()>& onPressedHandler)
+{
+	ButtonBlueprintModel buttonConfiguration = {
+		ButtonComponentModel{
+			.onPressedHandler = onPressedHandler,
 			.idleButtonSource = AssetPaths::Sprites::BUTTON_IDLE_IMAGE_PATH,
-			.hoveredButtonSource = AssetPaths::Sprites::BUTTON_HOVERED_IMAGE_PATH,
+			.hoveredButtonSource = AssetPaths::Sprites::BUTTON_PRESSED_IMAGE_PATH,
 			.pressedButtonSource = AssetPaths::Sprites::BUTTON_PRESSED_IMAGE_PATH,
-			.buttonSize = Vector2F{ 300.0f, 100.0f }
+			.buttonSize = Vector2F{ 275.0f, 80.0f }
 		},
-		TextMeshComponentModel
-		{
-			.fontSource = SharedResourcePaths::Fonts::CONSOLAS_FONT_PATH,
-			.text = "Single Player",
+		TextMeshComponentModel{
+			.fontSource = AssetPaths::Fonts::THE_BLAST_FONT_PATH,
+			.text = text,
+			.textColor = SDL_Color(255, 132, 31),
+			.fontSize = 100,
+			.textObjectInitialScale = Vector2F(0.25f, 0.25f),
 			.linesNumber = 1
 		}
 	};
 
-	auto button = GameObject::Instantiate<ButtonBlueprint>(buttonConfiguration);
+	return GameObject::Instantiate<ButtonBlueprint>(buttonConfiguration);
+}
 
-	button.lock()->GetTransform()->SetWorldPosition(Vector2F(Window::GetResolution().x * 0.5f, Window::GetResolution().y * 0.5f));
+void MainMenuScene::Enter()
+{
+	auto renderTargetSize = Renderer::GetResolutionTarget();
+
+	CreateBackground(renderTargetSize);
+	CreateTitle(renderTargetSize);
+	SetupLayoutGroup(renderTargetSize);
 }
