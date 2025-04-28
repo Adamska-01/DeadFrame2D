47ca9a3579361bb125d2c99540776d2bc63e3a63
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-28 19:47:55 +0100

    Added 'ImageScroller' to the engine project

---
+++ Core/2DGameEngine/src/Components/Rendering/ImageScroller.cpp
@@ -0,0 +1,104 @@
+#include "Components/Rendering/ImageScroller.h"
+#include "SubSystems/TextureManager.h"
+#include <Components/Transform.h>
+#include <Constants/ScreenConstants.h>
+#include <EventSystem/EventDispatcher.h>
+#include <EventSystem/Events/SubSystems/Renderer/RenderTargetSizeChangedEvent.h>
+#include <Tools/Helpers/EventHelpers.h>
+
+
+ImageScroller::ImageScroller(std::string_view textureSource, ScrollDirection scrollDirection, float scrollSpeed)
+	: Sprite(textureSource), scrollDirection(scrollDirection), scrollSpeed(scrollSpeed)
+{
+	renderTargetSize.x = ScreenConstants::DEFAULT_RENDERER_X_SIZE;
+	renderTargetSize.y = ScreenConstants::DEFAULT_RENDERER_Y_SIZE;
+
+	scrollOffset = 0;
+
+	EventDispatcher::RegisterEventHandler(std::type_index(typeid(RenderTargetSizeChangedEvent)), EventHelpers::BindFunction(this, &ImageScroller::RenderTargetSizeChangedHandler));
+}
+
+ImageScroller::~ImageScroller()
+{
+	EventDispatcher::DeregisterEventHandler(std::type_index(typeid(RenderTargetSizeChangedEvent)), EventHelpers::BindFunction(this, &ImageScroller::RenderTargetSizeChangedHandler));
+}
+
+void ImageScroller::RenderTargetSizeChangedHandler(std::shared_ptr<DispatchableEvent> dispatchableEvent)
+{
+	auto renderTargetChangeEvent = DispatchableEvent::SafeCast<RenderTargetSizeChangedEvent>(dispatchableEvent);
+
+	if (renderTargetChangeEvent == nullptr)
+		return;
+
+	renderTargetSize.x = renderTargetChangeEvent->renderTargetSize.x;
+	renderTargetSize.y = renderTargetChangeEvent->renderTargetSize.y;
+}
+
+void ImageScroller::Update(float deltaTime)
+{
+	scrollOffset += scrollSpeed * deltaTime;
+
+	auto scale = transform->GetWorldScale();
+	auto scaledTileWidth = static_cast<int>(spriteSize.x * scale.x);
+	auto scaledTileHeight = static_cast<int>(spriteSize.y * scale.y);
+
+	scrollOffset += scrollSpeed * deltaTime;
+
+	if (scrollDirection == ScrollDirection::HORIZONTAL)
+	{
+		if (scrollOffset >= scaledTileWidth)
+		{
+			scrollOffset -= scaledTileWidth;
+		}
+		else if (scrollOffset < 0)
+		{
+			scrollOffset += scaledTileWidth;
+		}
+	}
+	else
+	{
+		if (scrollOffset >= scaledTileHeight)
+		{
+			scrollOffset -= scaledTileHeight;
+		}
+		else if (scrollOffset < 0)
+		{
+			scrollOffset += scaledTileHeight;
+		}
+	}
+}
+
+void ImageScroller::Draw()
+{
+	if (spriteTexture == nullptr)
+		return;
+
+	auto position = transform->GetWorldPosition();
+	auto scale = transform->GetWorldScale();
+
+	// Calculate scaled size
+	auto scaledTileWidth = static_cast<int>(spriteSize.x * scale.x);
+	auto scaledTileHeight = static_cast<int>(spriteSize.y * scale.y);
+
+	// +2 to handle partial tiles at edges
+	auto tilesX = (renderTargetSize.x / scaledTileWidth) + 2;
+	auto tilesY = (renderTargetSize.y / scaledTileHeight) + 2;
+
+	auto isHorizontal = scrollDirection == ScrollDirection::HORIZONTAL;
+
+	for (auto y = 0; y < (isHorizontal ? 1 : tilesY); ++y)
+	{
+		for (auto x = 0; x < (isHorizontal ? tilesX : 1); ++x)
+		{
+			auto destRect = SDL_Rect
+			{
+				.x = x * scaledTileWidth - (isHorizontal ? static_cast<int>(scrollOffset) : 0) + static_cast<int>(position.x),
+				.y = y * scaledTileHeight - (isHorizontal ? 0 : static_cast<int>(scrollOffset)) + static_cast<int>(position.y),
+				.w = scaledTileWidth,
+				.h = scaledTileHeight
+			};
+
+			TextureManager::DrawTexture(spriteTexture, NULL, &destRect, transform->GetWorldRotation());
+		}
+	}
+}
--- Core/2DGameEngine/src/Components/Rendering/Sprite.cpp
+++ Core/2DGameEngine/src/Components/Rendering/Sprite.cpp
@@ -1,4 +1,4 @@
-#include "Components/Sprite.h"
+#include "Components/Rendering/Sprite.h"
 #include "Components/Transform.h"
 #include "GameObject.h"
 #include "SubSystems/TextureManager.h"
@@ -7,7 +7,7 @@
 Sprite::Sprite(std::string_view texturePath)
 {
 	transform = nullptr;
-	destRect = { 0, 0, 0, 0 };
+	spriteSize = Vector2I::One;
 
 	LoadSprite(texturePath);
 }
@@ -19,24 +19,21 @@ void Sprite::Init()
 
 void Sprite::Update(float deltaTime)
 {
-	auto worldPosition = transform->GetWorldPosition();
-	auto worldScale = transform->GetWorldScale();
-
-	destRect.x = round(worldPosition.x - (destRect.w * worldScale.x) / 2);
-	destRect.y = round(worldPosition.y - (destRect.h * worldScale.y) / 2);
 }
 
 void Sprite::Draw()
 {
-	if (!isActive)
-		return;
-	
+	auto worldPosition = transform->GetWorldPosition();
 	auto worldScale = transform->GetWorldScale();
 	auto worldRotation = transform->GetWorldRotation();
 
-	auto scaledDest = destRect;
-	scaledDest.w *= worldScale.x;
-	scaledDest.h *= worldScale.y;
+	auto scaledDest = SDL_Rect
+	{
+		static_cast<int>(worldPosition.x - (spriteSize.x * worldScale.x) / 2),
+		static_cast<int>(worldPosition.y - (spriteSize.y * worldScale.y) / 2),
+		static_cast<int>(spriteSize.x * worldScale.x),
+		static_cast<int>(spriteSize.y * worldScale.y)
+	};
 
 	TextureManager::DrawTexture(spriteTexture, NULL, &scaledDest, worldRotation);
 }
@@ -45,7 +42,7 @@ void Sprite::LoadSprite(std::string_view texturePath)
 {
 	spriteTexture = TextureManager::LoadTexture(texturePath);
 
-	SDL_QueryTexture(spriteTexture.get(), NULL, NULL, &destRect.w, &destRect.h);
+	SDL_QueryTexture(spriteTexture.get(), NULL, NULL, &spriteSize.x, &spriteSize.y);
 }
 
 std::shared_ptr<SDL_Texture> Sprite::GetTexture()
