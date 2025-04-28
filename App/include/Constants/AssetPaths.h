2f1b12f950a205c504d7e1730ffc5d3d0e8bfd8b
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-04-28 19:23:03 +0100

    Added more path sources

--- App/include/Constants/AssetPaths.h
+++ App/include/Constants/AssetPaths.h
@@ -11,8 +11,15 @@ namespace AssetPaths
 		inline constexpr std::string_view MULTIPLAYER_MAP_PATH = "App/Assets/Maps/MultiplayerMap.tmx";
 	}
 
+	namespace Backgrounds
+	{
+		inline constexpr std::string_view BACKGROUND_IMAGE_PATH = "App/Assets/Backgrounds/Stars.png";
+	}
+
 	namespace Sprites
 	{
+		inline constexpr std::string_view TITLE_IMAGE_PATH = "App/Assets/Sprites/Title.png";
+
 		inline constexpr std::string_view ARROW_IMAGE_PATH = "App/Assets/Sprites/Arrow.png";
 
 		inline constexpr std::string_view BUTTON_IDLE_IMAGE_PATH = "App/Assets/Sprites/Buttons/Button_Idle.png";
@@ -20,5 +27,10 @@ namespace AssetPaths
 		inline constexpr std::string_view BUTTON_HOVERED_IMAGE_PATH = "App/Assets/Sprites/Buttons/Button_Hovered.png";
 
 		inline constexpr std::string_view BUTTON_PRESSED_IMAGE_PATH = "App/Assets/Sprites/Buttons/Button_Pressed.png";
+	}
+
+	namespace Fonts
+	{
+		inline constexpr std::string_view THE_BLAST_FONT_PATH = "App/Assets/Fonts/The Blast.otf";
 	}
 }
