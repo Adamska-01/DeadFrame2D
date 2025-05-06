ed046f1249724b21336b1a36b7776cfa2f90eec9
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-06 18:55:26 +0200

    Set Box2D to v2.4.2

--- Scripts/Setup-Windows.bat
+++ Scripts/Setup-Windows.bat
@@ -4,6 +4,7 @@
 set "PREMAKE_PATH=Vendor\Premake\Windows\premake5.exe"
 set "PREMAKE_PATH_BASH=Vendor/Premake/Windows/premake5.exe"
 set "HOOK_PATH=../.git/hooks/post-checkout"
+set "BOX2D_SUBMODULE_PATH=../Vendor/Box2D/"
 
 
 :: Run Premake
@@ -26,5 +27,12 @@ echo Hook installed at %HOOK_PATH%
 
 :: Update submodules
 git submodule update --init --recursive
+
+:: Update Box2D Submodule to v2.4.2
+echo Updating Box2D submodule to v2.4.2...
+pushd "%BOX2D_SUBMODULE_PATH%"
+git checkout v2.4.2
+popd
+echo Box2D submodule updated to v2.4.2!
 
 pause
