995ead04e4dddfd0fd625e298c43624d0a0e3ab5
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-06 23:00:27 +0200

    Moved resources in 'Shared' project

--- Shared/include/Constants/PhysicsConstants.h
+++ Shared/include/Constants/PhysicsConstants.h
@@ -6,4 +6,8 @@ namespace PhysicsConstants
 	constexpr float GRAVITY_X = 0.0f;
 
 	constexpr float GRAVITY_Y = 9.81f;
+
+	constexpr int VELOCITY_ITERATIONS = 8;
+
+	constexpr int POSITION_ITERATIONS = 3;
 }
--- Shared/include/Constants/SharedResourcePaths.h
+++ Shared/include/Constants/SharedResourcePaths.h
@@ -7,5 +7,11 @@ namespace SharedResourcePaths
 	namespace Fonts
 	{
 		inline constexpr std::string_view CONSOLAS_FONT_PATH = "Shared/Resources/Fonts/consola.ttf";
+
+	}
+
+	namespace Configurations
+	{
+		inline constexpr std::string_view INPUT_CONTROLS_CONFIGURATION_PATH = "Shared/Resources/Configurations/InputControls.json";
 	}
 }
