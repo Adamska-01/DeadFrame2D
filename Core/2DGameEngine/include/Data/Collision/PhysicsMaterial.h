b88113a10f727f9fa4eb6ff410365a17fab519b1
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-12 19:40:28 +0200

    Added 'PhysicsMaterial'

---
+++ Core/2DGameEngine/include/Data/Collision/PhysicsMaterial.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Data/Collision/FilterData.h"
+
+
+class b2Shape;
+class b2PolygonShape;
+
+
+struct PhysicsMaterial
+{
+	b2Shape* shape = new b2PolygonShape();
+
+	float friction = 0.3f;
+	
+	float restitution = 0.0f;
+	
+	float restitutionThreshold = 1.0f;
+	
+	float density = 1.0f;
+	
+	bool isSensor = false;
+	
+	FilterData filter{};
+};
