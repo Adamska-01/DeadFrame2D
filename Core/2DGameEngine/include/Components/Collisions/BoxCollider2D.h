a37ce51952cea04e77fe4c9419317e35df01fd23
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-15 00:32:17 +0200

    Added Box2D support to 'BoxCollider2D'

--- Core/2DGameEngine/include/Components/Collisions/BoxCollider2D.h
+++ Core/2DGameEngine/include/Components/Collisions/BoxCollider2D.h
@@ -1,50 +1,16 @@
 #pragma once
 #include "Components/Collisions/Collider2D.h"
-#include "Factories/Abstractions/Debugging/IDebugColliderDrawer.h"
-#include <SDL_rect.h>
-
-
-struct CollisionInfo;
 
 
 class BoxCollider2D : public Collider2D
 {
 private:
-	SDL_Rect previousBox;
-
-	SDL_Rect box;
-
-	SDL_Rect cropOffset;
-
-	std::unique_ptr<IDebugColliderDrawer<BoxCollider2D>> debugCollisionDrawer;
-
-
-	void UpdatePreviousBox(float x, float y);
-
-	void SetBox(SDL_Rect& collisionBox, int x, int y, int w, int h);
+	b2PolygonShape* shape;
 
 
 public:
-	BoxCollider2D(SDL_Rect box, SDL_Rect cropOffset);
+	BoxCollider2D(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle, const PhysicsMaterial& physicsMaterial = PhysicsMaterial());
 
 
-	const SDL_Rect& GetCollisionBox() const;
-
-	const SDL_Rect& GetPreviousBox() const;
-
-	void SetBuffer(int x, int y, int w, int h);
-
-	void SetBox(int x, int y, int w, int h);
-
-
-	virtual void Draw() override;
-
-
-	virtual bool Accept(ICollisionVisitor& visitor, Collider2D* other) override;
-
-	virtual bool AcceptDispatch(BoxCollider2D* other, ICollisionVisitor& visitor) override;
-
-	virtual bool AcceptDispatch(CircleCollider2D* other, ICollisionVisitor& visitor) override;
-
-	virtual bool AcceptDispatch(TiledMapCompatibleCollider2D* other, ICollisionVisitor& visitor) override;
+	void SetNewBoxShape(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle);
 };
--- Core/2DGameEngine/src/Components/Collisions/BoxCollider2D.cpp
+++ Core/2DGameEngine/src/Components/Collisions/BoxCollider2D.cpp
@@ -1,82 +1,24 @@
 #include "Components/Collisions/BoxCollider2D.h"
-#include "Components/Transform.h"
-#include "Constants/CommonColors.h"
-#include "Factories/Concretions/Debugging/DebugBoxColliderDrawerFactory.h"
-#include "Tools/Collisions/ICollisionVisitor.h"
+#include "Tools/Helpers/Physics/PhysicsShapeCreators.h"
 
 
-BoxCollider2D::BoxCollider2D(SDL_Rect box, SDL_Rect cropOffset)
-	: previousBox(box), box(box), cropOffset(cropOffset)
+BoxCollider2D::BoxCollider2D(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle, const PhysicsMaterial& physicsMaterial)
+	: Collider2D(physicsMaterial), shape(nullptr)
 {
-	debugCollisionDrawer = std::unique_ptr<IDebugColliderDrawer<BoxCollider2D>>(std::move(DebugBoxColliderDrawerFactory().CreateProduct(this)));
+	SetNewBoxShape(boxSize, offsetFromCenter, initialAngle);
 }
 
-void BoxCollider2D::UpdatePreviousBox(float x, float y)
+void BoxCollider2D::SetNewBoxShape(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle)
 {
-	SetBox(previousBox, x, y, box.w, box.h);
-}
-
-const SDL_Rect& BoxCollider2D::GetCollisionBox() const
-{
-	return box;
-}
-
-const SDL_Rect& BoxCollider2D::GetPreviousBox() const
-{
-	return previousBox;
-}
-
-void BoxCollider2D::SetBox(SDL_Rect& collisionBox, int x, int y, int w, int h)
-{
-	collisionBox.x = x - cropOffset.x;
-	collisionBox.y = y - cropOffset.y;
-	collisionBox.w = static_cast<int>(std::round(w - cropOffset.w));
-	collisionBox.h = static_cast<int>(std::round(h - cropOffset.h));
-}
-
-void BoxCollider2D::SetBuffer(int x, int y, int w, int h)
-{
-	box.x = x;
-	box.y = y;
-	box.w = w;
-	box.h = h;
-
-	SetBox(box.x, box.y, box.w, box.h);
-}
-
-void BoxCollider2D::SetBox(int x, int y, int w, int h)
-{
-	SetBox(box, x, y, w, h);
-}
-
-void BoxCollider2D::Draw()
-{
-	Collider2D::Draw();
+	if (shape != nullptr)
+	{
+		delete shape;
+		shape = nullptr;
+	}
 	
-	debugCollisionDrawer->DrawCollider(CommonColors::WHITE);
-}
+	shape = PhysicsShapeCreators::CreateBoxShape(boxSize.x * 0.5f, boxSize.y * 0.5f, offsetFromCenter, initialAngle);
 
-bool BoxCollider2D::Accept(ICollisionVisitor& visitor, Collider2D* other)
-{
-	auto worldPosition = transform->GetWorldPosition();
+	this->physicsMaterial.shape = shape;
 
-	SetBox(worldPosition.x - (box.w / 2), worldPosition.y - (box.h / 2), box.w, box.h);
-	UpdatePreviousBox(startFramePosition.x - (box.w / 2), startFramePosition.y - (box.h / 2));
-
-	return other->AcceptDispatch(this, visitor);
-}
-
-bool BoxCollider2D::AcceptDispatch(BoxCollider2D* other, ICollisionVisitor& visitor)
-{
-	return visitor.Visit(this, other);
-}
-
-bool BoxCollider2D::AcceptDispatch(CircleCollider2D* other, ICollisionVisitor& visitor)
-{
-	return visitor.Visit(this, other);
-}
-
-bool BoxCollider2D::AcceptDispatch(TiledMapCompatibleCollider2D* other, ICollisionVisitor& visitor)
-{
-	return visitor.Visit(this, other);
+	MarkDirty();
 }
