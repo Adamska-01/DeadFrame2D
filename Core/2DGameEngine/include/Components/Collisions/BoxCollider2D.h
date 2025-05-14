4e543e75407c9567014a3ee7f22652adee67501b
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-15 00:38:38 +0200

    Added clean-up to 'BoxCollider2D'

--- Core/2DGameEngine/include/Components/Collisions/BoxCollider2D.h
+++ Core/2DGameEngine/include/Components/Collisions/BoxCollider2D.h
@@ -8,8 +8,13 @@ private:
 	b2PolygonShape* shape;
 
 
+	void DeleteShape();
+
+
 public:
 	BoxCollider2D(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle, const PhysicsMaterial& physicsMaterial = PhysicsMaterial());
+
+	virtual ~BoxCollider2D() override;
 
 
 	void SetNewBoxShape(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle);
--- Core/2DGameEngine/src/Components/Collisions/BoxCollider2D.cpp
+++ Core/2DGameEngine/src/Components/Collisions/BoxCollider2D.cpp
@@ -2,19 +2,29 @@
 #include "Tools/Helpers/Physics/PhysicsShapeCreators.h"
 
 
+void BoxCollider2D::DeleteShape()
+{
+	if (shape == nullptr)
+		return;
+
+	delete shape;
+	shape = nullptr;
+}
+
 BoxCollider2D::BoxCollider2D(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle, const PhysicsMaterial& physicsMaterial)
 	: Collider2D(physicsMaterial), shape(nullptr)
 {
 	SetNewBoxShape(boxSize, offsetFromCenter, initialAngle);
 }
 
+BoxCollider2D::~BoxCollider2D()
+{
+	DeleteShape();
+}
+
 void BoxCollider2D::SetNewBoxShape(Vector2F boxSize, Vector2F offsetFromCenter, float initialAngle)
 {
-	if (shape != nullptr)
-	{
-		delete shape;
-		shape = nullptr;
-	}
+	DeleteShape();
 	
 	shape = PhysicsShapeCreators::CreateBoxShape(boxSize.x * 0.5f, boxSize.y * 0.5f, offsetFromCenter, initialAngle);
 
