90e290d3a3f16e14214b30dd4f82ceab30dce8c0
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-16 16:28:19 +0200

    Added 'RigidBody2D' to the GameMap

--- App/src/Prefabs/GameMap.cpp
+++ App/src/Prefabs/GameMap.cpp
@@ -1,6 +1,7 @@
 #include "Components/GameMapParser.h"
 #include "Prefabs/GameMap.h"
 #include <Components/Collisions/Tile/Tiled/TiledMapCompatibleCollider2D.h>
+#include <Components/Physics/RigidBody2D.h>
 #include <Components/TileMap/Tiled/TiledMapCompatibleRenderer.h>
 
 
@@ -10,4 +11,9 @@ GameMap::GameMap(std::string_view mapSource, bool extendMapToRenderTarget)
 
 	tileRenderer = AddComponent<TiledMapCompatibleRenderer>(gameMapParser->RetrieveRenderMap(), extendMapToRenderTarget);
 	tileCollider = AddComponent<TiledMapCompatibleCollider2D>(gameMapParser->RetrieveCollisionMap());
+
+	auto bodyDef = BodyDefinition2D();
+	bodyDef.type = BodyType2D::Static;
+
+	AddComponent<RigidBody2D>(bodyDef);
 }
