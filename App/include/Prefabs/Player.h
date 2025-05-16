bbaa1254433722085bc81ad5ee3de9b929428143
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-16 16:32:23 +0200

    Removed redundant stuff in 'Player'

--- App/include/Prefabs/Player.h
+++ App/include/Prefabs/Player.h
@@ -1,25 +1,11 @@
 #pragma once
-#include "Generic/PlayerIdentifier.h"
 #include <GameObject.h>
-#include <Math/Vector2.h>
-
-
-class Sprite;
-class Cannon;
 
 
 class Player : public GameObject
 {
-private:
-	PlayerIdentifier playerID;
-
-	Sprite* sprite;
-
-	Cannon* cannon;
-
-
 public:
-	Player(PlayerIdentifier playerID, Vector2F scale);
+	Player();
 	
 	virtual ~Player() override = default;
 
