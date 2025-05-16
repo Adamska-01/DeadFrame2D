e8a8bde865a9dc1529f3998657595d696f4dba05
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2025-05-16 13:13:13 +0200

    Deleted unnecessary classes

--- App/src/Prefabs/LevelManagerObject.cpp
+++
@@ -1,8 +0,0 @@
-#include "Prefabs/LevelManagerObject.h"
-#include <Components/LevelManager.h>
-
-
-LevelManagerObject::LevelManagerObject(GameMode gameMode)
-{
-	AddComponent<LevelManager>(gameMode);
-}
