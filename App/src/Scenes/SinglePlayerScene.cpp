1c557937337ef3f74e071d7b698e315b3a8a3242
Adamska-01 <silverworld@hotmail.it>
2025-05-30 13:11:45 +0200

    Replaced everything with 'GameBoard' in 'SinglePlayerScene'

--- App/src/Scenes/SinglePlayerScene.cpp
+++ App/src/Scenes/SinglePlayerScene.cpp
@@ -1,20 +1,9 @@
-#include "Components/GameFlow/GameManager.h"
 #include "Constants/AssetPaths.h"
-#include "Prefabs/GameMap.h"
-#include "Prefabs/Player.h"
+#include "Prefabs/GameBoard.h"
 #include "Scenes/SinglePlayerScene.h"
-#include <Prefabs/BobbleGrid.h>
 
 
 void SinglePlayerScene::Enter()
 {
-	GameObject::Instantiate<GameMap>(AssetPaths::Maps::SINGLEPLAYER_MAP_PATH, true);
-
-	GameObject::Instantiate<Player>();
-
-	auto bobbleGridObject = GameObject::Instantiate<GameObject>();
-	bobbleGridObject.lock()->AddComponent<BobbleGrid>();
-
-	auto gameManagerObject = GameObject::Instantiate<GameObject>();
-	gameManagerObject.lock()->AddComponent<GameManager>();
+	GameObject::Instantiate<GameBoard>(AssetPaths::Maps::SINGLEPLAYER_MAP_PATH);
 }
