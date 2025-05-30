7e1daec69c1a58097c8c5cb59e1ebcf8a9d7922f
Adamska-01 <silverworld@hotmail.it>
2025-05-30 12:34:46 +0200

    Added 'TiledPropertyNames'

---
+++ Core/2DGameEngine/include/Constants/TiledPropertyNames.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string_view>
+
+
+namespace Constants::TiledPropertyNames
+{
+	inline constexpr std::string_view DENSITY = "density";
+	
+	inline constexpr std::string_view FRICTION = "friction";
+	
+	inline constexpr std::string_view IS_SENSOR = "isSensor";
+	
+	inline constexpr std::string_view RESTITUTION = "restitution";
+	
+	inline constexpr std::string_view RESTITUTION_THRESHOLD = "restitutionThreshold";
+}
