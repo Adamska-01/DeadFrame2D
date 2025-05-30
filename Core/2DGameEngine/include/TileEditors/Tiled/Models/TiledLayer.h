3408ef5778e79c94b5eff4c971c80052f1de6b49
Adamska-01 <silverworld@hotmail.it>
2025-05-30 12:37:17 +0200

    Added more stuff to 'TiledLayer'
    - 'name' field member
    - 'GetFloatProperty' function
    - 'GetBoolProperty' function

--- Core/2DGameEngine/include/TileEditors/Tiled/Models/TiledLayer.h
+++ Core/2DGameEngine/include/TileEditors/Tiled/Models/TiledLayer.h
@@ -3,11 +3,14 @@
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include <variant>
 #include <vector>
 
 
 struct TiledLayer
 {
+	std::string name;
+
 	std::unordered_map<std::string, TiledProperty> Properties;
 
 	std::vector<std::vector<int>> Data;
@@ -21,5 +24,25 @@ struct TiledLayer
 			return it->second;
 
 		return std::nullopt; 
+	}
+
+	float GetFloatProperty(std::string_view key, float defaultValue) const
+	{
+		auto prop = GetProperty(key);
+
+		if (prop && std::holds_alternative<float>(prop->Value))
+			return std::get<float>(prop->Value);
+		
+		return defaultValue;
+	}
+
+	bool GetBoolProperty(std::string_view key, bool defaultValue) const
+	{
+		auto prop = GetProperty(key);
+		
+		if (prop && std::holds_alternative<bool>(prop->Value))
+			return std::get<bool>(prop->Value);
+		
+		return defaultValue;
 	}
 };
