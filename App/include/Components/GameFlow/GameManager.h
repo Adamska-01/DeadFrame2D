e90bad0a59316c185998bcd255a2beeb19749e38
Adamska-01 <silverworld@hotmail.it>
2025-06-27 01:04:44 +0200

    Added more functionalities to the 'GameManager'

--- App/include/Components/GameFlow/GameManager.h
+++ App/include/Components/GameFlow/GameManager.h
@@ -1,13 +1,40 @@
 #pragma once
 #include <Components/GameComponent.h>
+#include <Coroutines/Task.h>
+#include <Generic/ScoreCounter.h>
 #include <string>
 #include <vector>
+
+
+class Cannon;
+class BobbleGrid;
+class GridCeiling;
+class MenuManager;
+class LevelTextController;
 
 
 class GameManager : public GameComponent
 {
 private:
 	std::vector<std::string> levelSources;
+
+	int tileSize;
+
+	int currentLevelIndex;
+
+	Cannon* player;
+
+	BobbleGrid* bobbleGrid;
+
+	GridCeiling* gridCeiling;
+
+	MenuManager* menuManager;
+
+	LevelTextController* levelTextController;
+
+	bool inputAllowed;
+
+	ScoreCounter scoreCounter;
 
 
 public:
@@ -18,7 +45,20 @@ public:
 
 	virtual void Init() override;
 
+	virtual void Start() override;
+
 	virtual void Update(float deltaTime) override;
 
 	virtual void Draw() override;
+
+
+	bool IsInputAllowed();
+
+	void SetScoreTextMesh(TextMesh* textMesh);
+
+	void AddScore(int scoreToAdd);
+
+	Task StartNextLevel(bool displayEndLevelText);
+
+	void ShowGameOverScreen();
 };
--- App/src/Components/GameFlow/GameManager.cpp
+++ App/src/Components/GameFlow/GameManager.cpp
@@ -3,27 +3,40 @@
 #include "Components/GameFlow/GameManager.h"
 #include "Components/GameMapParser.h"
 #include "Constants/AssetPaths.h"
+#include "Constants/BobbleConstants.h"
 #include "Constants/TiledObjectGroupNames.h"
+#include <Components/GridCeiling.h>
+#include <Components/MenuManager.h>
 #include <Components/Transform.h>
+#include <Components/UI/LevelTextController.h>
+#include <Coroutines/CoroutineScheduler.h>
 #include <Management/SceneManager.h>
 #include <TileEditors/Tiled/Models/TiledObjectGroup.h>
+#include <Tools/Helpers/Guards.h>
 #include <Tools/IO/FileSystemUtils.h>
 
 
 GameManager::GameManager()
+	: tileSize(32),
+	currentLevelIndex(-1),
+	player(nullptr),
+	bobbleGrid(nullptr),
+	gridCeiling(nullptr),
+	menuManager(nullptr),
+	levelTextController(nullptr),
+	inputAllowed(false)
 {
 	levelSources = FileSystemUtils::GetFilesInFolder(AssetPaths::Levels::SINGLEPLAYER_LEVELS_FOLDER_PATH);
 }
 
 void GameManager::Init()
 {
+	inputAllowed = false;
+
 	// Get Map Parser
 	auto gameMapParser = SceneManager::FindObjectOfType<GameMapParser>();
 	
-	if (gameMapParser == nullptr)
-	{
-		throw std::runtime_error("Cannon::Init: GameMapParser not found in the scene.");
-	}
+	Tools::Helpers::GuardAgainstNull(gameMapParser, "GameManager::Init: GameMapParser not found in the scene.");
 
 	// Get Player Position
 	auto groupObject = gameMapParser->RetrieveObjectGroup(TiledObjectGroupNames::PLAYER_POSITION_GROUP);
@@ -33,11 +46,11 @@ void GameManager::Init()
 		throw std::runtime_error("GameManager::Init: Player position group not found.");
 	}
 
-	auto player = OwningObject.lock()->GetComponentInChildren<Cannon>();
+	player = OwningObject.lock()->GetComponentInChildren<Cannon>();
 	player->GetGameObject().lock()->GetComponent<Transform>()->SetWorldPosition(groupObject.value().points.front());
 
 	// Generate first level
-	auto tileSize = gameMapParser->GetTileSize();
+	tileSize = gameMapParser->GetTileSize();
 	auto placements = gameMapParser->RetrieveObjectGroup(TiledObjectGroupNames::GRID_START_GROUP);
 
 	if (!placements.has_value())
@@ -45,15 +58,105 @@ void GameManager::Init()
 		throw std::runtime_error("GameManager::Init: Grid start object group not found.");
 	}
 
-	auto bobbleGrid = OwningObject.lock()->GetComponentInChildren<BobbleGrid>();
+	bobbleGrid = OwningObject.lock()->GetComponentInChildren<BobbleGrid>();
 	bobbleGrid->GetGameObject().lock()->GetComponent<Transform>()->SetWorldPosition(placements.value().points.front());
-	bobbleGrid->SetNewGridLevel(levelSources.front(), tileSize);
+
+	levelTextController = SceneManager::FindObjectOfType<LevelTextController>();
+
+	gridCeiling = OwningObject.lock()->GetComponentInChildren<GridCeiling>(true);
+
+	menuManager = SceneManager::FindObjectOfType<MenuManager>();
+
+	scoreCounter.Init();
+}
+
+void GameManager::Start()
+{
+	CoroutineScheduler::StartCoroutine(StartNextLevel(false));
 }
 
 void GameManager::Update(float deltaTime)
 {
+	if (!inputAllowed)
+		return;
+
+	scoreCounter.Update(deltaTime);
 }
 
 void GameManager::Draw()
 {
+}
+
+bool GameManager::IsInputAllowed()
+{
+	return inputAllowed;
+}
+
+void GameManager::SetScoreTextMesh(TextMesh* textMesh)
+{
+	scoreCounter.SetTextMesh(textMesh);
+}
+
+void GameManager::AddScore(int scoreToAdd)
+{
+	scoreCounter.AddScore(scoreToAdd);
+}
+
+Task GameManager::StartNextLevel(bool displayEndLevelText)
+{
+	inputAllowed = false;
+
+	currentLevelIndex++;
+
+	auto levelSize = levelSources.size();
+
+	if (currentLevelIndex >= levelSources.size())
+	{
+		// TODO: Show scoreboard
+		menuManager->ShowMenu(MenuID::VICTORY_MENU);
+
+		co_return;
+	};
+
+	if (displayEndLevelText)
+	{
+		auto completionTime = scoreCounter.GetCurrentTime();
+
+		auto bonusText = "\n" + std::to_string(static_cast<int>(completionTime)) + "sec\n";
+
+		if (completionTime <= BobbleConstants::MIN_TIME_FOR_BONUS_IN_SECONDS)
+		{
+			auto t = completionTime / BobbleConstants::MIN_TIME_FOR_BONUS_IN_SECONDS;
+			auto timeBonus = static_cast<int>(std::lerp(BobbleConstants::MAX_TIME_BONUS, 0, t));
+		
+			bonusText += "Time Bonus: +" + std::to_string(timeBonus);
+
+			scoreCounter.AddScore(timeBonus);
+		}
+		else
+		{
+			bonusText += "No Bonus";
+		}
+
+		co_await CoroutineScheduler::StartCoroutine(levelTextController->DisplayEndLevelText("Level " + std::to_string(currentLevelIndex) + " Completed!\n" + bonusText));
+	}
+
+	player->GetGameObject().lock()->GetTransform()->SetWorldRotation(0.0f);
+
+	bobbleGrid->SetNewGridLevel(levelSources[currentLevelIndex], tileSize);
+
+	player->ResetLoadedBobble();
+
+	gridCeiling->ResetCeiling();
+
+	co_await CoroutineScheduler::StartCoroutine(levelTextController->DisplayLevelText("Level " + std::to_string(currentLevelIndex)));
+
+	inputAllowed = true;
+
+	scoreCounter.ResetTimer();
+}
+
+void GameManager::ShowGameOverScreen()
+{
+	menuManager->ShowMenu(MenuID::GAME_OVER_MENU);
 }
