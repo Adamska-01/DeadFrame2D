b125e605025f79b5bdecd943e78d2f30a49e4fcd
Adamska-01 <silverworld@hotmail.it>
2025-07-01 14:04:37 +0200

    Added engine configuration source path

--- Shared/include/Constants/SharedResourcePaths.h
+++ Shared/include/Constants/SharedResourcePaths.h
@@ -7,11 +7,12 @@ namespace SharedResourcePaths
 	namespace Fonts
 	{
 		inline constexpr std::string_view CONSOLAS_FONT_PATH = "Shared/Resources/Fonts/consola.ttf";
-
 	}
 
 	namespace Configurations
 	{
 		inline constexpr std::string_view INPUT_CONTROLS_CONFIGURATION_PATH = "Shared/Resources/Configurations/InputControls.json";
+
+		inline constexpr std::string_view ENGINE_CONFIGURATION_PATH = "Shared/Resources/Configurations/Manifest.json";
 	}
 }
