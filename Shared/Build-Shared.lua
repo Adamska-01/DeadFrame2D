baf21a154310eea0d1c024e74897235c5c5172b6
Adamska-01 <silverworld@hotmail.it>
2025-07-03 21:37:15 +0200

    Adjusted premake file for the shared project (supports both debug and release builds now)

--- Shared/Build-Shared.lua
+++ Shared/Build-Shared.lua
@@ -1,12 +1,16 @@
 project "Shared"
 	kind "StaticLib"
 	language "C++" -- Doesn't really matter
+	cppdialect "C++17"
+	staticruntime "on"
+	
 	targetdir ("./Binaries/" .. OutputDir)
 	objdir ("./Binaries/Intermediates/" .. OutputDir)
 	debugdir "../"
 
 	files { 
-		"./include/**.*",
+		"./include/**.h",
+		"./src/**.cpp", 
 		"./Configurations/**.*",
 		"./Resources/**.*"
 	}
@@ -15,3 +19,15 @@ project "Shared"
 		"include",
 		"../Vendor/nlohmann-3.11.3"
 	}
+	
+	filter {}
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "RELEASE" }
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
