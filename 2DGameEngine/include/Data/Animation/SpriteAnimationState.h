5772b1290212fb1ce0ea56bb3149212432883ace
Adamska-01 <silverworld@hotmail.it>
2025-07-14 11:13:23 +0200

    Added 'SpriteAnimationState'

---
+++ 2DGameEngine/include/Data/Animation/SpriteAnimationState.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SDL_render.h>
+
+
+/** @brief Runtime state of an animation. */
+struct SpriteAnimationState
+{
+	/** @brief Current frame index (can be fractional for smooth interpolation). */
+	float currentFrame = 0.0f;
+
+	/** @brief Indicates whether the animation has started playing. */
+	bool started = false;
+
+	/** @brief Optional flip state. */
+	SDL_RendererFlip flipState = SDL_RendererFlip::SDL_FLIP_NONE;
+};
