c6fe6893e876eb7eb8056fbb5c30fdfec6391001
Adamska-01 <silverworld@hotmail.it>
2025-07-15 01:27:18 +0200

    Added 'AudioClipBlueprint'

---
+++ 2DGameEngine/include/Blueprints/Audio/AudioClipBlueprint.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <GameObject.h>
+#include <Math/Vector2.h>
+#include <string_view>
+
+
+class AudioSource;
+
+
+class AudioClipBlueprint : public GameObject
+{
+private:
+	AudioSource* audioSource;
+
+	std::string_view audioSourcePath;
+
+	Vector2F position;
+
+	float volume;
+
+	bool isMusic;
+
+	bool loop;
+
+
+public:
+	AudioClipBlueprint(
+		const std::string_view& audioSourcePath,
+		Vector2F position = Vector2F::Zero,
+		float volume = 1.0f,
+		bool isMusic = false,
+		bool loop = false);
+
+
+	virtual void ConstructGameObject() override;
+
+
+	AudioSource* GetAudioSource();
+};
