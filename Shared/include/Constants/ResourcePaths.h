e49948c65bca60ef1462a9754acdb35bd316de02
Adamska-01 <silverworld@hotmail.it>
2025-07-19 19:52:37 +0200

    Added the CollisionMasks path in 'ResourcePaths'

--- Shared/include/Constants/ResourcePaths.h
+++ Shared/include/Constants/ResourcePaths.h
@@ -15,6 +15,8 @@ namespace Constants::ResourcePaths
 
 		inline constexpr std::string_view SHARED_ENGINE_CONFIG_ALIAS = "shared_engine_config";
 
+		inline constexpr std::string_view SHARED_PHYSICS_CONFIGS_ALIAS = "shared_physics_config";
+
 		inline constexpr std::string_view LOGOS_ALIAS = "logos";
 
 
@@ -23,6 +25,8 @@ namespace Constants::ResourcePaths
 		inline static const ResourceMount SHARED_INPUT_CONTROLS{ std::string(SHARED_INPUT_CONTROLS_ALIAS), "Shared/Resources/Configurations/" };
 
 		inline static const ResourceMount SHARED_ENGINE_CONFIG{std::string(SHARED_ENGINE_CONFIG_ALIAS), "Shared/Resources/Configurations/" };
+
+		inline static const ResourceMount SHARED_PHYSICS_CONFIGS{std::string(SHARED_PHYSICS_CONFIGS_ALIAS), "Shared/Resources/Configurations/Physics/" };
 
 		inline static const ResourceMount LOGOS{std::string(LOGOS_ALIAS), "Shared/Resources/Logos/" };
 	}
@@ -34,6 +38,8 @@ namespace Constants::ResourcePaths
 		static const ResolvedPath INPUT_CONTROLS{ std::string(Folders::SHARED_INPUT_CONTROLS_ALIAS), "InputControls.json" };
 
 		static const ResolvedPath ENGINE_CONFIGURATION{ std::string(Folders::SHARED_ENGINE_CONFIG_ALIAS), "Manifest.json" };
+
+		static const ResolvedPath COLLISION_MASKS{ std::string(Folders::SHARED_PHYSICS_CONFIGS_ALIAS), "CollisionMasks.json" };
 
 		static const ResolvedPath WINDOW_ICON{ std::string(Folders::LOGOS_ALIAS), "Window_Icon.png" };
 
