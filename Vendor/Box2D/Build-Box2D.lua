58d2ce101c03019619696571bed1529d7f09869e
Adamska-01 <silverworld@hotmail.it>
2025-10-06 22:57:34 +0200

    Turned off static runtime on Box2D

--- Vendor/Box2D/Build-Box2D.lua
+++ Vendor/Box2D/Build-Box2D.lua
@@ -2,7 +2,7 @@ project "Box2D"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
-	staticruntime "on"
+	staticruntime "off"
 	defines { "_Static_assert=static_assert" }
 
 	targetdir ("./Binaries/" .. OutputDir)
