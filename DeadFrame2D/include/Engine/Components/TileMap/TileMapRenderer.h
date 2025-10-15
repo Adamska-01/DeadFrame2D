e18a014cf42c4638f2f8401b4d3ffd6273c12148
Adamska-01 <silverworld@hotmail.it>
2025-10-15 11:41:34 +0200

    Removed TODO comment from 'TileMapRenderer'

--- DeadFrame2D/include/Engine/Components/TileMap/TileMapRenderer.h
+++ DeadFrame2D/include/Engine/Components/TileMap/TileMapRenderer.h
@@ -7,7 +7,6 @@ namespace DeadFrame2D::Engine
 	template <typename TMap>
 	class TileMapRenderer : public GameComponent
 	{
-		// TODO: Check if this works correctly with "IsA"
 		TYPE_INFO(TileMapRenderer<TMap>, GameComponent);
 
 
