1cd923d2424ccfb3c8734947cc315708c3a8dcd6
Adamska-01 <silverworld@hotmail.it>
2025-11-06 12:55:02 +0100

    Added cpp file for 'ComponentEntry'

---
+++ DeadFrame2D/src/Engine/Entity/ComponentEntry.cpp
@@ -0,0 +1,10 @@
+#include "Engine/Components/GameComponent.h"
+#include "Engine/Entity/ComponentEntry.h"
+
+
+namespace DeadFrame2D::Engine
+{
+	ComponentEntry::ComponentEntry() = default;
+
+	ComponentEntry::~ComponentEntry() = default;
+}
