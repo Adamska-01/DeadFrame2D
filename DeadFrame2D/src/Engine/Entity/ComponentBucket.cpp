f9da8f874f62b768dbdcc10d2bf16a8c0f6519e3
Adamska-01 <silverworld@hotmail.it>
2025-11-06 12:54:43 +0100

    Added cpp file for 'ComponentBucket'

---
+++ DeadFrame2D/src/Engine/Entity/ComponentBucket.cpp
@@ -0,0 +1,63 @@
+#include "Engine/Components/GameComponent.h"
+#include "Engine/Entity/ComponentBucket.h"
+#include "Engine/Entity/GameObject.h"
+#include "Engine/Entity/Handles/GameObject/ObjectHandle.h"
+#include <cassert>
+
+
+namespace DeadFrame2D::Engine
+{
+	ComponentBucket::ComponentBucket()
+	{
+		components.clear();
+	}
+
+	ComponentBucket::~ComponentBucket()
+	{
+		for (auto& cmp : components)
+		{
+			cmp.alive = false;
+			cmp.generation++;
+			cmp.instance.reset();
+		}
+
+		components.clear();
+	}
+
+
+	GameComponent* ComponentBucket::GetAt(uint32_t index) const
+	{
+		assert(index < components.size());
+
+		return components[index].instance.get();
+	}
+
+	uint32_t ComponentBucket::FindFreeSlot()
+	{
+		for (size_t i = 0; i < components.size(); ++i)
+		{
+			if (!components[i].alive)
+				return static_cast<uint32_t>(i);
+		}
+
+		components.emplace_back();
+
+		return static_cast<uint32_t>(components.size() - 1);
+	}
+
+	void ComponentBucket::LinkComponentToOwner(const ObjectHandle<GameObject>& owner, GameComponent* toInitialize)
+	{
+		toInitialize->SetGameObject(owner);
+		toInitialize->RegisterAllHandlers(owner);
+	}
+
+	bool ComponentBucket::IsValid(uint32_t index, uint32_t generation) const
+	{
+		if (index >= components.size())
+			return false;
+
+		const auto& entry = components[index];
+
+		return entry.alive && entry.generation == generation;
+	}
+}
