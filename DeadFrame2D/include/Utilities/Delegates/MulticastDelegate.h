c495ca2735af11b3c8f97892eb58eab5dad46aec
Adamska-01 <silverworld@hotmail.it>
2025-11-20 23:05:34 +0100

    Refactored 'MulticastDelegate' (made it more type-safe by adding support for handle binding)

--- DeadFrame2D/include/Utilities/Delegates/MulticastDelegate.h
+++ DeadFrame2D/include/Utilities/Delegates/MulticastDelegate.h
@@ -1,7 +1,17 @@
 #pragma once
+#include "Utilities/Delegates/Listeners/Abstractions/IListener.h"
+#include <algorithm>
 #include <cstdint>
 #include <functional>
-#include <unordered_map>
+#include <vector>
+#include <memory>
+
+
+namespace DeadFrame2D::Engine
+{
+	class ObjectHandleBase;
+	class ComponentHandleBase;
+}
 
 
 namespace DeadFrame2D::Utilities
@@ -10,8 +20,12 @@ namespace DeadFrame2D::Utilities
 	class MulticastDelegate
 	{
 	private:
-		// TODO: it should be a list of std::function paired with their identifier.
-		std::unordered_map<uintptr_t, std::function<void(Args...)>> listeners;
+		std::vector<std::unique_ptr<IListener<Args...>>> listeners;
+
+		ListenerID nextID = 0;
+
+
+		void PruneExpired();
 
 
 	public:
@@ -19,54 +33,221 @@ namespace DeadFrame2D::Utilities
 
 		~MulticastDelegate() = default;
 
+		MulticastDelegate(const MulticastDelegate&) = delete;
 
-		bool IsEmpty();
+		MulticastDelegate& operator=(const MulticastDelegate&) = delete;
 
+		MulticastDelegate(MulticastDelegate&&) noexcept = default;
+
+		MulticastDelegate& operator=(MulticastDelegate&&) noexcept = default;
+
+
+		template<typename T>
+		ListenerID AddRaw(T* instance, void (T::* func)(Args...));
+
+		template<typename T>
+		ListenerID AddShared(const std::shared_ptr<T>& instance, void (T::* func)(Args...));
+
+		/**
+		* Adds a functor. This is generally used for lambda functions that do not capture pointers.
+		*/
+		ListenerID AddLambda(std::function<void(Args...)> func);
+
+		ListenerID AddHandle(const DeadFrame2D::Engine::ObjectHandleBase& handle, std::function<void(Args...)> func);
+
+		ListenerID AddHandle(const DeadFrame2D::Engine::ComponentHandleBase& handle, std::function<void(Args...)> func);
+
+
+		/**
+		 * @brief Invokes all active listeners.
+		 */
+		void Broadcast(const Args& ...args);
+
+		/**
+		 * @brief Removes a specific listener by comparing identity (e.g., owner pointer).
+		 */
+		template<typename T>
+		void RemoveByListener(const T* listener);
+
+		/**
+		 * @brief Removes a listener with a specific ID.
+		 */
+		void RemoveByID(ListenerID id);
+
+		/**
+		 * @brief Clears the entire delegate list.
+		 */
 		void Clear();
 
+		/**
+		 * @brief Returns number of active (non-expired) listeners.
+		 */
+		size_t Count() const;
 
-		void RegisterCallback(const std::function<void(Args...)>& func, uintptr_t identifier);
-
-		void DeregisterCallback(uintptr_t identifier);
-
-
-		void operator() (Args... params);
+		/**
+		 * @brief Returns true if there are no listeners.
+		 */
+		bool IsEmpty() const;
 	};
 
 
 	using MultiCastVoid = MulticastDelegate<>;
+}
+
+
+#include "Engine/Entity/Handles/GameObject/ObjectHandleBase.h"
+#include "Engine/Entity/ComponentHandleBase.h"
+#include "Utilities/Delegates/Listeners/HandleListener.h"
+#include "Utilities/Delegates/Listeners/LambdaListener.h"
+#include "Utilities/Delegates/Listeners/RawListener.h"
+#include "Utilities/Delegates/Listeners/SharedPtrListener.h"
+
+
+namespace DeadFrame2D::Utilities
+{
+	template<typename ...Args>
+	inline void MulticastDelegate<Args...>::PruneExpired()
+	{
+		listeners.erase(
+			std::remove_if(
+				listeners.begin(),
+				listeners.end(),
+				[](const std::unique_ptr<IListener<Args...>>& listener)
+				{
+					return listener->IsExpired();
+				}),
+			listeners.end());
+	}
 
 
 	template<typename ...Args>
-	inline bool MulticastDelegate<Args...>::IsEmpty()
+	template<typename T>
+	inline ListenerID MulticastDelegate<Args...>::AddRaw(T* instance, void(T::* func)(Args...))
 	{
-		return listeners.empty();
+		auto id = nextID++;
+
+		auto listener = std::make_unique<RawListener<T, Args...>>(instance, func);
+		listener->SetID(id);
+		
+		listeners.push_back(std::move(listener));
+
+		return id;
 	}
 
 	template<typename ...Args>
-	inline void MulticastDelegate<Args...>::Clear()
+	template<typename T>
+	inline ListenerID MulticastDelegate<Args...>::AddShared(const std::shared_ptr<T>& instance, void(T::* func)(Args...))
+	{
+		auto id = nextID++;
+		
+		auto listener = std::make_unique<SharedPtrListener<T, Args...>>(instance, func);
+		listener->SetID(id);
+
+		listeners.push_back(std::move(listener));
+
+		return id;
+	}
+
+	template<typename ...Args>
+	inline ListenerID MulticastDelegate<Args...>::AddLambda(std::function<void(Args...)> func)
+	{
+		auto id = nextID++;
+		
+		auto listener = std::make_unique<LambdaListener<Args...>>(std::move(func));
+		listener->SetID(id);
+
+		listeners.push_back(std::move(listener));
+
+		return id;
+	}
+
+	template<typename ...Args>
+	inline ListenerID MulticastDelegate<Args...>::AddHandle(const DeadFrame2D::Engine::ObjectHandleBase& handle, std::function<void(Args...)> func)
+	{
+		auto id = nextID++;
+
+		auto listener = std::make_unique<HandleListener<Args...>>(handle, std::move(func));
+		listener->SetID(id);
+
+		listeners.push_back(std::move(listener));
+
+		return id;
+	}
+
+	template<typename ...Args>
+	inline ListenerID MulticastDelegate<Args...>::AddHandle(const DeadFrame2D::Engine::ComponentHandleBase& handle, std::function<void(Args...)> func)
+	{
+		auto id = nextID++;
+
+		auto listener = std::make_unique<HandleListener<Args...>>(handle, std::move(func));
+		listener->SetID(id);
+
+		listeners.push_back(std::move(listener));
+
+		return id;
+	}
+
+	template<typename ...Args>
+	inline void MulticastDelegate<Args...>::Broadcast(const Args& ...args)
+	{
+		PruneExpired();
+
+		for (auto& listener : listeners)
+		{
+			listener->Invoke(args...);
+		}
+	}
+
+	template<typename ...Args>
+	template<typename T>
+	inline void MulticastDelegate<Args...>::RemoveByListener(const T* listener)
+	{
+		listeners.erase(
+			std::remove_if(
+				listeners.begin(), 
+				listeners.end(),
+				[&](const auto& l) 
+				{
+					return l->Matches(*listener);
+				}),
+			listeners.end());
+	}
+
+	template<typename ...Args>
+	inline void MulticastDelegate<Args...>::RemoveByID(ListenerID id)
+	{
+		listeners.erase(
+			std::remove_if(
+				listeners.begin(), 
+				listeners.end(),
+				[id](const auto& l)
+				{
+					return l->GetID() == id;
+				}),
+			listeners.end());
+	}
+
+	template<typename ...Args>
+	void MulticastDelegate<Args...>::Clear()
 	{
 		listeners.clear();
 	}
 
 	template<typename ...Args>
-	inline void MulticastDelegate<Args...>::RegisterCallback(const std::function<void(Args...)>& func, uintptr_t identifier)
+	inline size_t MulticastDelegate<Args...>::Count() const
 	{
-		listeners[identifier] = func;
+		return std::count_if(
+			listeners.begin(),
+			listeners.end(),
+			[](const auto& l)
+			{
+				return !l->IsExpired();
+			});
 	}
 
 	template<typename ...Args>
-	inline void MulticastDelegate<Args...>::DeregisterCallback(uintptr_t identifier)
+	inline bool MulticastDelegate<Args...>::IsEmpty() const
 	{
-		listeners.erase(identifier);
-	}
-
-	template<typename ...Args>
-	inline void MulticastDelegate<Args...>::operator()(Args ...params)
-	{
-		for (const auto& [_, listener] : listeners)
-		{
-			listener(params...);
-		}
+		return Count() == 0;
 	}
 }
