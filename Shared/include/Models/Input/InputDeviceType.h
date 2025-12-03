42f9d8e42479d87a6cd6ed7f5de210ebacc19869
Adamska-01 <silverworld@hotmail.it>
2025-12-03 14:49:55 +0100

    Fixed serialization in 'InputDeviceType'

--- Shared/include/Models/Input/InputDeviceType.h
+++ Shared/include/Models/Input/InputDeviceType.h
@@ -18,8 +18,8 @@ namespace Shared::Models
 	// Serialization functions (nlohmann)
 	NLOHMANN_JSON_SERIALIZE_ENUM(InputDeviceType,
 	{
-		{InputDeviceType::KEYBOARD, "KEYBOARD"},
-		{InputDeviceType::MOUSE, "MOUSE"},
-		{InputDeviceType::CONTROLLER, "CONTROLLER"}
+		{InputDeviceType::KEYBOARD, "Keyboard"},
+		{InputDeviceType::MOUSE, "Mouse"},
+		{InputDeviceType::CONTROLLER, "Controller"}
 	})
 }
