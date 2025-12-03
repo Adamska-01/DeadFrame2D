af382c486e7b45fbb5313897336890169d7b4667
Adamska-01 <silverworld@hotmail.it>
2025-12-03 14:47:14 +0100

    Added "Shared::Models::InputAction"

---
+++ Shared/include/Models/Input/ActionMap/InputAction.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Models/Input/ActionMap/Properties/InputInteraction.h"
+#include "Models/Input/ActionMap/Properties/InputProcessor.h"
+#include "Models/Input/ActionMap/Types/ActionType.h"
+#include "Models/Input/ActionMap/Types/ValueType.h"
+#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+
+
+namespace Shared::Models
+{
+	struct InputAction
+	{
+		std::string name;
+
+		ActionType type = ActionType::BUTTON;
+
+		ValueType valueType = ValueType::ANY;
+
+
+		std::vector<InputProcessor> processors;
+
+		std::vector<InputInteraction> interactions;
+	};
+
+
+	inline void to_json(nlohmann::json& j, const InputAction& a)
+	{
+		j["name"] = a.name;
+		j["type"] = a.type;
+		j["valueType"] = a.valueType;
+
+		if (!a.processors.empty())
+		{
+			j["processors"] = a.processors;
+		}
+		if (!a.interactions.empty())
+		{
+			j["interactions"] = a.interactions;
+		}
+	}
+
+	inline void from_json(const nlohmann::json& j, InputAction& a)
+	{
+		j.at("name").get_to(a.name);
+		j.at("type").get_to(a.type);
+		j.at("valueType").get_to(a.valueType);
+
+		if (j.contains("processors"))
+		{
+			a.processors = j.at("processors").get<std::vector<InputProcessor>>();
+		}
+		if (j.contains("interactions"))
+		{
+			a.interactions = j.at("interactions").get<std::vector<InputInteraction>>();
+		}
+	}
+}
