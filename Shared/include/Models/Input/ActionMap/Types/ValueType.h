e8a23f5c4a24868b3cde0a17c9331403f30dabc2
Adamska-01 <silverworld@hotmail.it>
2025-12-03 14:42:46 +0100

    Added "Shared::Models::ValueType"

---
+++ Shared/include/Models/Input/ActionMap/Types/ValueType.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <nlohmann/json.hpp>
+
+
+namespace Shared::Models
+{
+	enum class ValueType
+	{
+		ANY,
+
+		BOOL,
+
+		FLOAT,
+
+		VECTOR2
+	};
+
+
+	NLOHMANN_JSON_SERIALIZE_ENUM(ValueType,
+	{
+		{ ValueType::ANY, "Any" },
+		{ ValueType::BOOL, "Bool" },
+		{ ValueType::FLOAT, "Float" },
+		{ ValueType::VECTOR2, "Vector2" }
+	});
+}
