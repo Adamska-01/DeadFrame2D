585fff78d642ae4fd2dec8ec60e919515ecc6b81
Adamska-01 <silverworld@hotmail.it>
2025-12-03 14:57:26 +0100

    Added 'DefaultDeviceNames' constants

---
+++ DeadFrame2D/include/Data/Input/DefaultDeviceNames.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace DeadFrame2D::Data
+{
+	namespace DefaultDeviceNames
+	{
+		inline constexpr const char* KEYBOARD = "Keyboard";
+
+		inline constexpr const char* MOUSE = "Mouse";
+
+		inline constexpr const char* CONTROLLER = "Controller";
+	}
+}
