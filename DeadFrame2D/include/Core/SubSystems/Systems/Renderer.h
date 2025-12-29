7254437d28d7f75cf475aa29aa525f1443460530
Adamska-01 <silverworld@hotmail.it>
2025-12-30 00:02:59 +0100

    Added render functions to the 'Renderer' class

--- DeadFrame2D/include/Core/SubSystems/Systems/Renderer.h
+++ DeadFrame2D/include/Core/SubSystems/Systems/Renderer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Constants/CommonColors.h"
 #include "Core/Math/Vector2.h"
 #include "Core/SubSystems/Abstractions/ISubSystem.h"
 #include "DF2D_API.h"
@@ -49,6 +50,25 @@ namespace DeadFrame2D::Core
 
 
 	public:
+		static void DrawPixel(const Vector2F& pixelPos, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE);
+		
+		static void DrawLine(const Vector2F& p1, const Vector2F& p2, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE);
+
+		static void DrawRect(SDL_Rect rect, float angleDegrees, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE, bool filled = false);
+
+		static void DrawCircle(const Vector2F& center, float radius, SDL_Color color, bool filled);
+
+		static void DrawTexture(
+			SDL_Texture* texture,
+			const SDL_Rect* srcRect = NULL,
+			const SDL_FRect* dstRect = NULL,
+			float angle = 0.0f,
+			SDL_FPoint* rotationOrigin = NULL,
+			SDL_RendererFlip flip = SDL_FLIP_NONE,
+			Uint8 alpha = 255,
+			SDL_Color colorMod = DeadFrame2D::Constants::CommonColors::WHITE);
+
+
 		static void ClearAndPresentBuffer();
 
 		static void BeginCamera(DeadFrame2D::Engine::Camera* camera);
--- DeadFrame2D/src/Core/SubSystems/Systems/Renderer.cpp
+++ DeadFrame2D/src/Core/SubSystems/Systems/Renderer.cpp
@@ -1,3 +1,4 @@
+#include "Constants/MathConstants.h"
 #include "Core/Debugging/Debug.h"
 #include "Core/SubSystems/Systems/Renderer.h"
 #include "Core/SubSystems/Systems/Window.h"
@@ -12,6 +13,7 @@ namespace DeadFrame2D::Core
 	using namespace Shared::Models;
 
 	using namespace DeadFrame2D::Engine;
+	using namespace DeadFrame2D::Constants;
 
 
 	SDL_Renderer* Renderer::renderer = nullptr;
@@ -82,6 +84,206 @@ namespace DeadFrame2D::Core
 	void Renderer::EndDraw()
 	{
 
+	}
+
+	void Renderer::DrawPixel(const Vector2F& pixelPos, SDL_Color color)
+	{
+		if (renderer == nullptr)
+			return;
+
+		auto previousBackgroundColor = GetDisplayColor();
+
+		SetDisplayColor(color.r, color.g, color.b, color.a);
+
+		SDL_RenderDrawPointF(renderer, pixelPos.x, pixelPos.y);
+
+		SetDisplayColor(previousBackgroundColor.r, previousBackgroundColor.g, previousBackgroundColor.b, previousBackgroundColor.a);
+	}
+
+	void Renderer::DrawLine(const Vector2F& p1, const Vector2F& p2, SDL_Color color)
+	{
+		if (renderer == nullptr)
+			return;
+
+		auto previousBackgroundColor = Renderer::GetDisplayColor();
+
+		SetDisplayColor(color.r, color.g, color.b, color.a);
+
+		SDL_RenderDrawLineF(renderer, p1.x, p1.y, p2.x, p2.y);
+
+		SetDisplayColor(previousBackgroundColor.r, previousBackgroundColor.g, previousBackgroundColor.b, previousBackgroundColor.a);
+	}
+
+	void Renderer::DrawRect(SDL_Rect rect, float angleDegrees, SDL_Color color, bool filled)
+	{
+		if (renderer == nullptr)
+			return;
+
+		const auto cx = rect.x + rect.w * 0.5f;
+		const auto cy = rect.y + rect.h * 0.5f;
+
+		const auto angleRad = angleDegrees * (MathConstants::PI_f / 180.0f);
+		const auto cosA = cosf(angleRad);
+		const auto sinA = sinf(angleRad);
+
+		// Local corners relative to center
+		SDL_FPoint corners[4] =
+		{
+			{-rect.w / 2.0f, -rect.h / 2.0f},
+			{ rect.w / 2.0f, -rect.h / 2.0f},
+			{ rect.w / 2.0f,  rect.h / 2.0f},
+			{-rect.w / 2.0f,  rect.h / 2.0f}
+		};
+
+		// Rotate and translate corners
+		for (auto& p : corners)
+		{
+			auto x = p.x * cosA - p.y * sinA + cx;
+			auto y = p.x * sinA + p.y * cosA + cy;
+			
+			p.x = x;
+			p.y = y;
+		}
+
+		if (filled)
+		{
+			SDL_Vertex vertices[6] =
+			{
+				{corners[0], color, {0,0}},
+				{corners[1], color, {0,0}},
+				{corners[2], color, {0,0}},
+				{corners[2], color, {0,0}},
+				{corners[3], color, {0,0}},
+				{corners[0], color, {0,0}}
+			};
+
+			SDL_RenderGeometry(renderer, nullptr, vertices, 6, nullptr, 0);
+		}
+		else
+		{
+			const SDL_Color oldColor = GetDisplayColor();
+
+			SetDisplayColor(color.r, color.g, color.b, color.a);
+
+			for (auto i = 0; i < 4; ++i)
+			{
+				SDL_RenderDrawLineF(
+					renderer,
+					corners[i].x, 
+					corners[i].y,
+					corners[(i + 1) % 4].x, 
+					corners[(i + 1) % 4].y);
+			}
+
+			SetDisplayColor(oldColor.r, oldColor.g, oldColor.b, oldColor.a);
+		}
+	}
+
+	void Renderer::DrawCircle(const Vector2F& center, float radius, SDL_Color color, bool filled)
+	{
+		if (renderer == nullptr)
+			return;
+
+		// Increase segments for smoother circles
+		// TODO: Make this a constant somewhere
+		const auto segments = 64;
+
+		if (filled)
+		{
+			std::vector<SDL_Vertex> vertices(segments + 2);
+			std::vector<int> indices(segments * 3);
+
+			vertices[0] = { {center.x, center.y}, color, {0.5f, 0.5f} };
+
+			// Vertices
+			for (auto i = 0; i <= segments; ++i)
+			{
+				auto angle = (2.0f * MathConstants::PI_f * i) / segments;
+				vertices[i + 1] =
+				{
+					.position = { center.x + cosf(angle) * radius, center.y + sinf(angle) * radius },
+					.color = color,
+					.tex_coord = { (cosf(angle) + 1.0f) * 0.5f, (sinf(angle) + 1.0f) * 0.5f }
+				};
+			}
+
+			// Indices (Each triangle connects the center to 2 outer points)
+			for (auto i = 0; i < segments; ++i)
+			{
+				indices[i * 3 + 0] = 0;
+				indices[i * 3 + 1] = i + 1;
+				indices[i * 3 + 2] = i + 2;
+			}
+
+			SDL_RenderGeometry(renderer, nullptr, vertices.data(), (int)vertices.size(), indices.data(), (int)indices.size());
+		}
+		else
+		{
+			auto oldRenderColor = GetDisplayColor();
+
+			SetDisplayColor(color.r, color.g, color.b, color.a);
+
+			std::vector<SDL_FPoint> points(segments + 1);
+
+			for (auto i = 0; i <= segments; ++i)
+			{
+				auto angle = (2.0f * MathConstants::PI_f * i) / segments;
+
+				points[i].x = center.x + cosf(angle) * radius;
+				points[i].y = center.y + sinf(angle) * radius;
+			}
+
+			SDL_RenderDrawLinesF(renderer, points.data(), segments + 1);
+
+			SetDisplayColor(oldRenderColor.r, oldRenderColor.g, oldRenderColor.b, oldRenderColor.a);
+		}
+	}
+
+	void Renderer::DrawTexture(SDL_Texture* texture, const SDL_Rect* srcRect, const SDL_FRect* dstRect, float angle, SDL_FPoint* rotationOrigin, SDL_RendererFlip flip, Uint8 alpha, SDL_Color colorMod)
+	{
+		if (texture == nullptr || renderer == nullptr)
+			return;
+
+		// Determine destination rectangle
+		SDL_FRect dest{};
+		if (dstRect)
+		{
+			dest = *dstRect;
+		}
+		else
+		{
+			int w, h;
+			SDL_QueryTexture(texture, nullptr, nullptr, &w, &h);
+
+			dest = SDL_FRect{ 0.0f, 0.0f, static_cast<float>(w), static_cast<float>(h) };
+		}
+
+		Uint8 oldAlpha, oldR, oldG, oldB;
+		SDL_GetTextureAlphaMod(texture, &oldAlpha);
+		SDL_GetTextureColorMod(texture, &oldR, &oldG, &oldB);
+
+		SDL_SetTextureAlphaMod(texture, alpha);
+		SDL_SetTextureColorMod(texture, colorMod.r, colorMod.g, colorMod.b);
+
+		// Determine rotation origin
+		SDL_FPoint fallbackOrigin
+		{
+			dest.w * 0.5f,
+			dest.h * 0.5f
+		};
+
+		SDL_RenderCopyExF(
+			renderer,
+			texture,
+			srcRect,
+			&dest,
+			angle,
+			rotationOrigin ? rotationOrigin : &fallbackOrigin,
+			flip);
+
+		// Restore texture state
+		SDL_SetTextureAlphaMod(texture, oldAlpha);
+		SDL_SetTextureColorMod(texture, oldR, oldG, oldB);
 	}
 
 	void Renderer::ClearAndPresentBuffer()
