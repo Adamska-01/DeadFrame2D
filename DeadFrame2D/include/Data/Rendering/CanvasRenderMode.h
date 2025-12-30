dd73fe9431312de99f78ce1b20f5f8fbd357037c
Adamska-01 <silverworld@hotmail.it>
2025-12-30 02:57:22 +0100

    Added 'CanvasRenderMode' enum

---
+++ DeadFrame2D/include/Data/Rendering/CanvasRenderMode.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+namespace DeadFrame2D::Data
+{
+	/**
+	 * @brief Defines how a canvas is rendered relative to the screen or camera.
+	 */
+	enum class CanvasRenderMode
+	{
+		/**
+		 * @brief Renders the canvas directly in screen space, relative to the window.
+		 */
+		SCREEN_SPACE_OVERLAY,
+
+		/**
+		 * @brief Renders the canvas in screen space but relative to a specific camera.
+		 */
+		SCREEN_SPACE_CAMERA
+	};
+}
