11e2a16f41852be254d19945791b09d8fb89bf0e
Adamska-01 <silverworld@hotmail.it>
2026-01-03 01:31:28 +0100

    Added 'RenderTask' struct

---
+++ DeadFrame2D/include/Data/Rendering/Pipeline/RenderTask.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Data/Rendering/Pipeline/RenderData.h"
+#include "Data/Rendering/Pipeline/RenderPhase.h"
+#include "Data/Rendering/Pipeline/RenderPrimitive.h"
+#include "Data/Rendering/Pipeline/RenderSortKey.h"
+#include "Engine/Entity/ComponentHandle.h"
+
+
+namespace DeadFrame2D::Engine
+{
+	class Canvas;
+}
+
+
+namespace DeadFrame2D::Data
+{
+	struct RenderTask
+	{
+		friend class RenderSystem;
+
+
+	private:
+		RenderSortKey renderSortKey;
+
+
+	public:
+		RenderPhase renderPhase;
+
+		RenderPrimitive renderType;
+
+		RenderData renderData;
+
+		int sortOrder;
+
+		DeadFrame2D::Engine::ComponentHandle<DeadFrame2D::Engine::Canvas> canvas;
+
+
+		inline uint64_t GetSortKey() const
+		{
+			return renderSortKey;
+		}
+	};
+}
