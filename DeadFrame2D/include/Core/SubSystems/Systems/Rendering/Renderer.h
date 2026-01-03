c1a017deab7cfec4432ebde5cf097b967eb8f2f4
Adamska-01 <silverworld@hotmail.it>
2026-01-03 01:30:32 +0100

    Moved Renderer.h

--- DeadFrame2D/include/Core/SubSystems/Systems/Rendering/Renderer.h
+++ DeadFrame2D/include/Core/SubSystems/Systems/Rendering/Renderer.h
@@ -2,7 +2,9 @@
 #include "Constants/CommonColors.h"
 #include "Core/Math/Vector2.h"
 #include "Core/SubSystems/Abstractions/ISubSystem.h"
+#include "Core/SubSystems/Systems/Rendering/RenderSystem.h"
 #include "DF2D_API.h"
+#include "Engine/Entity/ComponentHandle.h"
 #include <cstdint>
 #include <Models/Rendering/RendererConfig.h>
 #include <SDL.h>
@@ -16,15 +18,13 @@ namespace DeadFrame2D::Engine
 
 namespace DeadFrame2D::Core
 {
-	class DF2D_API Renderer : public ISubSystem
+	class DF2D_API Renderer : protected RenderSystem, public ISubSystem
 	{
 		friend class SubSystemManager;
 
 
 	private:
 		static SDL_Renderer* renderer;
-
-		static DeadFrame2D::Engine::Camera* activeCamera;
 
 
 		Renderer(SDL_Window* window, const Shared::Models::RendererConfig& config);
@@ -54,7 +54,7 @@ namespace DeadFrame2D::Core
 		
 		static void DrawLine(const Vector2F& p1, const Vector2F& p2, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE);
 
-		static void DrawRect(SDL_Rect rect, float angleDegrees, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE, bool filled = false);
+		static void DrawRect(SDL_FRect rect, float angleDegrees, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE, bool filled = false);
 
 		static void DrawCircle(const Vector2F& center, float radius, SDL_Color color, bool filled);
 
@@ -62,22 +62,18 @@ namespace DeadFrame2D::Core
 			SDL_Texture* texture,
 			const SDL_Rect* srcRect = NULL,
 			const SDL_FRect* dstRect = NULL,
+			const SDL_FPoint* rotationOrigin = NULL,
 			float angle = 0.0f,
-			SDL_FPoint* rotationOrigin = NULL,
 			SDL_RendererFlip flip = SDL_FLIP_NONE,
-			Uint8 alpha = 255,
 			SDL_Color colorMod = DeadFrame2D::Constants::CommonColors::WHITE);
+
+
+		static void DrawFromTask(DeadFrame2D::Data::RenderTask& renderTask, DeadFrame2D::Engine::ComponentHandle<DeadFrame2D::Engine::Camera> camera = {}, bool requiresSreenSpaceConversion = true);
 
 
 		static void ClearAndPresentBuffer();
 
-		static void BeginCamera(DeadFrame2D::Engine::Camera* camera);
-
-		static void EndCamera();
-
 		static SDL_Renderer* GetRenderer();
-
-		static DeadFrame2D::Engine::Camera* GetActiveCamera();
 
 		static SDL_Color GetDisplayColor();
 
