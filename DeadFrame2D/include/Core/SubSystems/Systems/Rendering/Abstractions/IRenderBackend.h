4793a8cbb39c4e14cdde58b2fdd4d937d9dea286
Adamska-01 <silverworld@hotmail.it>
2026-01-04 23:01:01 +0100

    Added 'IRenderBackend' interface

---
+++ DeadFrame2D/include/Core/SubSystems/Systems/Rendering/Abstractions/IRenderBackend.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Constants/CommonColors.h"
+#include "Core/Math/Vector2.h"
+#include "DF2D_API.h"
+#include <SDL_render.h>
+
+
+namespace DeadFrame2D::Core
+{
+	class DF2D_API IRenderBackend
+	{
+	public:
+		virtual void DrawPixel(const Vector2F& pixelPos, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE) = 0;
+
+		virtual void DrawLine(const Vector2F& p1, const Vector2F& p2, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE) = 0;
+
+		virtual void DrawRect(SDL_FRect rect, float angleDegrees, SDL_Color color = DeadFrame2D::Constants::CommonColors::WHITE, bool filled = false) = 0;
+
+		virtual void DrawCircle(const Vector2F& center, float radius, SDL_Color color, bool filled) = 0;
+
+		virtual void DrawTexture(
+			SDL_Texture* texture,
+			const SDL_Rect* srcRect = NULL,
+			const SDL_FRect* dstRect = NULL,
+			const SDL_FPoint* rotationOrigin = NULL,
+			float angle = 0.0f,
+			SDL_RendererFlip flip = SDL_FLIP_NONE,
+			SDL_Color colorMod = DeadFrame2D::Constants::CommonColors::WHITE) = 0;
+
+
+		virtual void SetRenderTarget(SDL_Texture* renderTarget) = 0;
+
+		virtual void ClearCurrentRenderTarget() = 0;
+	};
+}
