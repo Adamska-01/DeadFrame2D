0e6c4f0a2107981e698aa9635f075b4f90809cef
Adamska-01 <silverworld@hotmail.it>
2026-01-08 23:07:03 +0100

    Added 'LineBatchRenderResolver' for batch rendering

---
+++ DeadFrame2D/include/Core/SubSystems/Systems/Rendering/Resolvers/Functors/LineBatchRenderResolver.h
@@ -0,0 +1,67 @@
+#pragma once
+#include "Core/Math/Vector2.h"
+#include "Core/SubSystems/Systems/Rendering/Abstractions/IRenderBackend.h"
+#include "Data/Rendering/Pipeline/Shapes/LineBatchRenderData.h"
+#include "Engine/Components/Rendering/Camera.h"
+#include "Engine/Entity/ComponentHandle.h"
+#include "Utilities/Collisions/CollisionUtils.h"
+#include <optional>
+
+
+namespace DeadFrame2D::Core
+{
+	struct LineBatchRenderResolver
+	{
+		// Render
+		void operator()(
+			IRenderBackend& renderBackend,
+			const DeadFrame2D::Data::LineBatchRenderData& renderData,
+			DeadFrame2D::Engine::ComponentHandle<DeadFrame2D::Engine::Camera> camera,
+			bool requiresScreenSpaceConversion) const
+		{
+			for (const auto& line : renderData.lineBatch)
+			{
+				Vector2F p1 = line.p1;
+				Vector2F p2 = line.p2;
+
+				if (camera != nullptr && requiresScreenSpaceConversion)
+				{
+					p1 = camera->WorldToScreen(p1);
+					p2 = camera->WorldToScreen(p2);
+				}
+
+				renderBackend.DrawLine(p1, p2, line.color);
+			}
+		}
+
+		// Visibility Check
+		std::optional<DeadFrame2D::Data::LineBatchRenderData> operator()(
+			const DeadFrame2D::Data::LineBatchRenderData& renderData,
+			DeadFrame2D::Engine::ComponentHandle<DeadFrame2D::Engine::Camera> camera) const
+		{
+			using namespace DeadFrame2D::Data;
+			using namespace DeadFrame2D::Utilities;
+
+
+			// Always visible if no camera (screen space)
+			if (camera == nullptr)
+				return renderData;
+
+			auto filtered = LineBatchRenderData();
+			filtered.lineBatch.reserve(renderData.lineBatch.size());
+
+			for (const auto& line : renderData.lineBatch)
+			{
+				auto screenP1 = camera->WorldToScreen(line.p1);
+				auto screenP2 = camera->WorldToScreen(line.p2);
+
+				if (Collision::SegmentVsRect(screenP1, screenP2, camera->GetViewBox()))
+				{
+					filtered.lineBatch.push_back(line);
+				}
+			}
+
+			return filtered.lineBatch.empty() ? std::nullopt : std::optional{ std::move(filtered) };
+		}
+	};
+}
