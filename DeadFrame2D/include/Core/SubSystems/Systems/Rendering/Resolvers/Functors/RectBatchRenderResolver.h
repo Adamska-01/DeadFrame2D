76d1f0c06303650782793142be0bd3d0607dca5a
Adamska-01 <silverworld@hotmail.it>
2026-01-09 01:37:16 +0100

    Added 'RectBatchRenderResolver'

---
+++ DeadFrame2D/include/Core/SubSystems/Systems/Rendering/Resolvers/Functors/RectBatchRenderResolver.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "Core/Math/Vector2.h"
+#include "Core/SubSystems/Systems/Rendering/Abstractions/IRenderBackend.h"
+#include "Data/Rendering/Pipeline/Shapes/RectBatchRenderData.h"
+#include "Engine/Components/Rendering/Camera.h"
+#include "Engine/Entity/ComponentHandle.h"
+#include "Utilities/Collisions/CollisionUtils.h"
+#include <optional>
+
+
+namespace DeadFrame2D::Core
+{
+	struct RectBatchRenderResolver
+	{
+		// Render
+		void operator()(
+			IRenderBackend& renderBackend,
+			const DeadFrame2D::Data::RectBatchRenderData& renderData,
+			DeadFrame2D::Engine::ComponentHandle<DeadFrame2D::Engine::Camera> camera,
+			bool requiresScreenSpaceConversion) const
+		{
+			for (const auto& rect : renderData.rectBatch)
+			{
+				SDL_FRect destRect = rect.destRect;
+
+				if (camera != nullptr && requiresScreenSpaceConversion)
+				{
+					auto screenPos = camera->WorldToScreen(Vector2F(destRect.x, destRect.y));
+
+					destRect.x = screenPos.x;
+					destRect.y = screenPos.y;
+					destRect.w *= camera->GetZoom();
+					destRect.h *= camera->GetZoom();
+				}
+
+				renderBackend.DrawRect(
+					destRect,
+					rect.rotation,
+					rect.color,
+					rect.filled);
+			}
+		}
+
+		// Visibility Check
+		std::optional<DeadFrame2D::Data::RectBatchRenderData> operator()(
+			const DeadFrame2D::Data::RectBatchRenderData& renderData,
+			DeadFrame2D::Engine::ComponentHandle<DeadFrame2D::Engine::Camera> camera) const
+		{
+			using namespace DeadFrame2D::Data;
+			using namespace DeadFrame2D::Utilities;
+
+
+			// Always visible if no camera (screen space)
+			if (camera == nullptr)
+				return renderData;
+
+			auto filtered = RectBatchRenderData();
+			filtered.rectBatch.reserve(renderData.rectBatch.size());
+
+			for (const auto& rect : renderData.rectBatch)
+			{
+				SDL_FRect screenRect = rect.destRect;
+
+				auto screenPos = camera->WorldToScreen(Vector2F(screenRect.x, screenRect.y));
+
+				screenRect.x = screenPos.x;
+				screenRect.y = screenPos.y;
+				screenRect.w *= camera->GetZoom();
+				screenRect.h *= camera->GetZoom();
+
+				if (Collision::RectVsRect(screenRect, camera->GetViewBox()))
+				{
+					filtered.rectBatch.push_back(rect);
+				}
+			}
+
+			return filtered.rectBatch.empty() ? std::nullopt : std::optional{ std::move(filtered) };
+		}
+	};
+}
