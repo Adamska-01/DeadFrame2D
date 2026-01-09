d0352c46432d4c74c02e2e925fa26d3905d25c09
Adamska-01 <silverworld@hotmail.it>
2026-01-09 17:31:58 +0100

    Added 'DefaultSortOrders' constants

---
+++ DeadFrame2D/include/Constants/Rendering/DefaultSortOrders.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SDL_pixels.h>
+
+
+namespace DeadFrame2D::Constants::DefaultSortOrders
+{
+	constexpr int SPRITE_RENDERER = 0;
+
+	constexpr int TILE_RENDERER = -1;
+
+	constexpr int UI_RENDERER = 0;
+
+	constexpr int DEBUG_RENDERER = 9999;
+}
