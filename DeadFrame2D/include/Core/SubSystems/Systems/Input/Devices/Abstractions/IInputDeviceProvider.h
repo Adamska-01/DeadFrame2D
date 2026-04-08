b14dd897fbf357a91b0b91ffbda49d01dea60db2
Adamska-01 <silverworld@hotmail.it>
2026-04-08 22:23:52 +0200

    Added 'IInputDeviceProvider' abstraction

---
+++ DeadFrame2D/include/Core/SubSystems/Systems/Input/Devices/Abstractions/IInputDeviceProvider.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "DF2D_API.h"
+#include "Data/Input/InputDeviceID.h"
+#include <vector>
+
+
+namespace DeadFrame2D::Core
+{
+	class InputDevice;
+	class KeyboardInputDevice;
+	class MouseInputDevice;
+	class ControllerInputDevice;
+
+
+	class DF2D_API IInputDeviceProvider
+	{
+	public:
+		virtual ~IInputDeviceProvider() = default;
+
+
+		virtual InputDevice* GetDevice(DeadFrame2D::Data::InputDeviceID id) = 0;
+
+		virtual std::vector<InputDevice*> GetAllDevices() const = 0;
+
+		virtual KeyboardInputDevice* Keyboard() = 0;
+
+		virtual MouseInputDevice* Mouse() = 0;
+
+		virtual ControllerInputDevice* Controller(DeadFrame2D::Data::InputDeviceID id) = 0;
+
+		virtual ControllerInputDevice* CurrentController() = 0;
+	};
+}
