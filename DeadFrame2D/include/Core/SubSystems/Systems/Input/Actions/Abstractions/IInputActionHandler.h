c4a1e20d10139872d7caf80087d88647603059f6
Adamska-01 <silverworld@hotmail.it>
2026-04-08 22:25:26 +0200

    Added 'IInputActionHandler' abstraction

---
+++ DeadFrame2D/include/Core/SubSystems/Systems/Input/Actions/Abstractions/IInputActionHandler.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "DF2D_API.h"
+#include <Models/Input/ActionMap/Types/InputControlType.h>
+
+
+namespace DeadFrame2D::Core
+{
+	class InputDevice;
+
+
+	class DF2D_API IInputActionHandler
+	{
+	public:
+		virtual ~IInputActionHandler() = default;
+
+
+		virtual void ProcessBinding(const InputDevice& device, Shared::Models::InputControlType inputControlType, int controlID) = 0;
+	};
+}
