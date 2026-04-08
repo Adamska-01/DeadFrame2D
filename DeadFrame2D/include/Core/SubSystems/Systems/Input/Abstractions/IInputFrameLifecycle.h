26a27211589f90ff1e7874c00cf5fb3b11c4c705
Adamska-01 <silverworld@hotmail.it>
2026-04-08 22:18:28 +0200

    Added 'IInputFrameLifecycle'

---
+++ DeadFrame2D/include/Core/SubSystems/Systems/Input/Abstractions/IInputFrameLifecycle.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "DF2D_API.h"
+
+
+namespace DeadFrame2D::Core
+{
+	class InputDevice;
+
+
+	class DF2D_API IInputFrameLifecycle
+	{
+	public:
+		virtual ~IInputFrameLifecycle() = default;
+
+
+		virtual void BeginFrame() = 0;
+
+		virtual void PreUpdate() = 0;
+	};
+}
