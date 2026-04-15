058922e4fb01697b43990735287a35ffa6ae80de
Adamska-01 <silverworld@hotmail.it>
2026-04-16 00:17:24 +0200

    Added a premake file for the test project

---
+++ Tests/Build-Tests.lua
@@ -0,0 +1,80 @@
+project "Tests"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("./Binaries/" .. OutputDir)
+	objdir ("./Binaries/Intermediates/" .. OutputDir)
+	debugdir "../"
+
+	defines { "DF2D_DYNAMIC", "DF2D_DYNAMIC_BUILD" }
+
+	dependson { "DeadFrame2D", "Box2D", "tinyxml2" }
+
+	files { 
+		"**.cpp",
+	}
+
+	includedirs { 
+		"include",
+		"../DeadFrame2D/include",
+		"../DeadFrame2D/Internal",
+		"../Vendor/nlohmann-3.11.3", 
+		"../Vendor/tinyxml2-10.0.0", 
+		"../Vendor/Box2D/Module/include",
+		"../Vendor/SDL/SDL2-2.30.10/include", 
+		"../Vendor/SDL/SDL2_image-2.8.2/include", 
+		"../Vendor/SDL/SDL2_ttf-2.22.0/include", 
+		"../Vendor/SDL/SDL2_mixer-2.8.0/include",
+		"../Vendor/Doctest/Module"
+	}
+
+	links { "DeadFrame2D", "SDL2", "SDL2_image", "SDL2_ttf", "SDL2_mixer", "Box2D", "tinyxml2" }
+
+
+	function configure_platform_libs(osName, arch)
+		libdirs(table.join(
+			get_sdl_libdirs("../Vendor/SDL/", osName, arch),
+			get_libdir("../DeadFrame2D/Binaries/"),
+			get_libdir("../Vendor/Box2D/Binaries/"),
+			get_libdir("../Vendor/tinyxml2-10.0.0/Binaries/")
+		))
+	end
+
+
+	filter "platforms:x86"
+		architecture "x86"
+
+	filter "platforms:x64"
+		architecture "x64"
+
+
+	filter "system:windows"
+		implibdir ("./Binaries/" .. OutputDir)
+
+	filter { "system:windows", "platforms:x86" }
+		configure_platform_libs("Windows", "x86")
+	
+	filter { "system:windows", "platforms:x64" }
+		configure_platform_libs("Windows", "x64")
+
+
+	filter { "system:linux", "platforms:x86" }
+		configure_platform_libs("Linux", "x86")
+	
+	filter { "system:linux", "platforms:x64" }
+		configure_platform_libs("Linux", "x64")
+
+
+	filter {}
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "RELEASE" }
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
