df90a0d546278e0228b639a2b78d31bee8db3d66
Adamska-01 <silverworld@hotmail.it>
2026-06-06 21:35:10 +0200

    Added 'ITextureBackend' to abstract texture loading

---
+++ DeadFrame2D/include/Core/Context/Systems/Graphics/Abstractions/ITextureBackend.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Core/Math/Vector2.h"
+#include "Data/Systems/Graphics/TextureID.h"
+#include "DF2D_API.h"
+#include <string>
+
+
+namespace DF2D::Core
+{
+	class DF2D_API ITextureBackend
+	{
+	public:
+		virtual ~ITextureBackend() = default;
+
+
+		virtual Data::TextureID LoadFromFile(const std::string& filename) = 0;
+
+		virtual void UnloadTexture(Data::TextureID id) = 0;
+
+		virtual Core::Vector2I GetTextureSize(Data::TextureID id) = 0;
+
+		virtual void* GetNativeHandle(Data::TextureID id) = 0;
+	};
+}
