b4df9337d42c0a1c079473f444d54d136b0d3bdb
Adamska-01 <silverworld@hotmail.it>
2026-06-07 23:48:37 +0200

    Made 'CommonColors' use the 'Color' type

--- DeadFrame2D/include/Constants/CommonColors.h
+++ DeadFrame2D/include/Constants/CommonColors.h
@@ -1,50 +1,50 @@
 #pragma once
-#include <SDL_pixels.h>
+#include "Core/Math/Color.h"
 
 
 namespace DF2D::Constants::CommonColors
 {
-	inline constexpr SDL_Color WHITE = { 255, 255, 255, 255 };
+	inline constexpr Core::Color WHITE = { 255, 255, 255, 255 };
 
-	inline constexpr SDL_Color RED = { 255, 0, 0, 255 };
+	inline constexpr Core::Color RED = { 255, 0, 0, 255 };
 
-	inline constexpr SDL_Color GREEN = { 0, 255, 0, 255 };
+	inline constexpr Core::Color GREEN = { 0, 255, 0, 255 };
 
-	inline constexpr SDL_Color BLUE = { 0, 0, 255, 255 };
+	inline constexpr Core::Color BLUE = { 0, 0, 255, 255 };
 
-	inline constexpr SDL_Color YELLOW = { 255, 255, 0, 255 };
+	inline constexpr Core::Color YELLOW = { 255, 255, 0, 255 };
 
-	inline constexpr SDL_Color CYAN = { 0, 255, 255, 255 };
+	inline constexpr Core::Color CYAN = { 0, 255, 255, 255 };
 
-	inline constexpr SDL_Color MAGENTA = { 255, 0, 255, 255 };
+	inline constexpr Core::Color MAGENTA = { 255, 0, 255, 255 };
 
-	inline constexpr SDL_Color BLACK = { 0, 0, 0, 255 };
+	inline constexpr Core::Color BLACK = { 0, 0, 0, 255 };
 
-	inline constexpr SDL_Color GRAY = { 169, 169, 169, 255 };
+	inline constexpr Core::Color GRAY = { 169, 169, 169, 255 };
 
-	inline constexpr SDL_Color DARK_GRAY = { 105, 105, 105, 255 };
+	inline constexpr Core::Color DARK_GRAY = { 105, 105, 105, 255 };
 
-	inline constexpr SDL_Color LIGHT_GRAY = { 211, 211, 211, 255 };
+	inline constexpr Core::Color LIGHT_GRAY = { 211, 211, 211, 255 };
 
-	inline constexpr SDL_Color ORANGE = { 255, 165, 0, 255 };
+	inline constexpr Core::Color ORANGE = { 255, 165, 0, 255 };
 
-	inline constexpr SDL_Color PURPLE = { 128, 0, 128, 255 };
+	inline constexpr Core::Color PURPLE = { 128, 0, 128, 255 };
 
-	inline constexpr SDL_Color BROWN = { 139, 69, 19, 255 };
+	inline constexpr Core::Color BROWN = { 139, 69, 19, 255 };
 
-	inline constexpr SDL_Color PINK = { 255, 192, 203, 255 };
+	inline constexpr Core::Color PINK = { 255, 192, 203, 255 };
 
-	inline constexpr SDL_Color LIGHT_BLUE = { 173, 216, 230, 255 };
+	inline constexpr Core::Color LIGHT_BLUE = { 173, 216, 230, 255 };
 
-	inline constexpr SDL_Color LIME = { 0, 255, 0, 255 };
+	inline constexpr Core::Color LIME = { 0, 255, 0, 255 };
 
-	inline constexpr SDL_Color INDIGO = { 75, 0, 130, 255 };
+	inline constexpr Core::Color INDIGO = { 75, 0, 130, 255 };
 
-	inline constexpr SDL_Color TURQUOISE = { 64, 224, 208, 255 };
+	inline constexpr Core::Color TURQUOISE = { 64, 224, 208, 255 };
 
-	inline constexpr SDL_Color TEAL = { 0, 128, 128, 255 };
+	inline constexpr Core::Color TEAL = { 0, 128, 128, 255 };
 
-	inline constexpr SDL_Color IVORY = { 255, 255, 240, 255 };
+	inline constexpr Core::Color IVORY = { 255, 255, 240, 255 };
 
-	inline constexpr SDL_Color SALMON = { 250, 128, 114, 255 };
+	inline constexpr Core::Color SALMON = { 250, 128, 114, 255 };
 }
