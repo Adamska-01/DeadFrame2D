678c2050af2816b90b6c347e99c238bda60e1252
Adamska-01 <silverworld@hotmail.it>
2026-06-10 22:45:09 +0200

    Added rendering conversions for SDL structs

---
+++ DeadFrame2D/Internal/include/Converters/Rendering/RenderingConversions.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "Core/Math/Color.h"
+#include "Core/Math/Rect.h"
+#include "Data/Systems/Rendering/RenderFlip.h"
+#include <SDL_pixels.h>
+#include <SDL_rect.h>
+#include <SDL_render.h>
+
+
+namespace DF2D::Internal::RenderingConversions
+{
+	inline SDL_Color ToSDLColor(const Core::Color& c)
+	{
+		return SDL_Color{ c.r, c.g, c.b, c.a };
+	}
+
+	inline Core::Color ToColor(const SDL_Color& c)
+	{
+		return Core::Color{ c.r, c.g, c.b, c.a };
+	}
+
+
+	inline SDL_FRect ToSDLFRect(const Core::RectF& r)
+	{
+		return SDL_FRect{ r.x, r.y, r.w, r.h };
+	}
+
+	inline Core::RectF ToRectF(const SDL_FRect& r)
+	{
+		return Core::RectF{ r.x, r.y, r.w, r.h };
+	}
+
+
+	inline SDL_Rect ToSDLRect(const Core::RectI& r)
+	{
+		return SDL_Rect{ r.x, r.y, r.w, r.h };
+	}
+
+	inline Core::RectI ToRectI(const SDL_Rect& r)
+	{
+		return Core::RectI{ r.x, r.y, r.w, r.h };
+	}
+
+
+	inline SDL_RendererFlip ToSDLRenderFlip(Data::RenderFlip f)
+	{
+		switch (f)
+		{
+		case Data::RenderFlip::HORIZONTAL:	return SDL_FLIP_HORIZONTAL;
+		case Data::RenderFlip::VERTICAL:	return SDL_FLIP_VERTICAL;
+		default:							return SDL_FLIP_NONE;
+		}
+	}
+
+	inline Data::RenderFlip ToRenderFlip(SDL_RendererFlip f)
+	{
+		switch (f)
+		{
+		case SDL_FLIP_HORIZONTAL:	return Data::RenderFlip::HORIZONTAL;
+		case SDL_FLIP_VERTICAL:		return Data::RenderFlip::VERTICAL;
+		default:					return Data::RenderFlip::NONE;
+		}
+	}
+}
