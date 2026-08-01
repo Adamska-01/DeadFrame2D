a6c0482dddad8c5142553a61a2e8682d992cf839
Adamska-01 <silverworld@hotmail.it>
2026-08-01 20:42:09 +0200

    Added 'IPhysicsDebugDraw' interface

---
+++ DeadFrame2D/include/Core/Context/Systems/Physics/Abstractions/IPhysicsDebugDraw.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Core/Math/Color.h"
+#include "Core/Math/Vector2.h"
+#include "DF2D_API.h"
+
+
+namespace DF2D::Core
+{
+	/**
+	 * @brief Receives debug-draw primitives from a physics backend, expressed in engine types only (pixels).
+	 */
+	class DF2D_API IPhysicsDebugDraw
+	{
+	public:
+		virtual ~IPhysicsDebugDraw() = default;
+
+
+		virtual void DrawSegment(const Vector2F& p1, const Vector2F& p2, const Color& color) = 0;
+
+		virtual void DrawCircle(const Vector2F& center, float radius, const Color& color) = 0;
+
+		virtual void DrawPoint(const Vector2F& position, float size, const Color& color) = 0;
+	};
+}
