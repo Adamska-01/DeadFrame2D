300568b2f49cb2e5dd33f6edbd6377eb3db00778
Adamska-01 <silverworld@hotmail.it>
2026-08-01 15:13:53 +0200

    Replaced b2Shape pointer with 'ShapeDefinition2D' in 'PhysicsMaterial'

--- DeadFrame2D/include/Data/Components/Collision/PhysicsMaterial.h
+++ DeadFrame2D/include/Data/Components/Collision/PhysicsMaterial.h
@@ -1,24 +1,24 @@
 #pragma once
 #include "Data/Components/Collision/FilterData.h"
-#include <box2d/box2d.h>
+#include "Data/Components/Collision/Shapes/ShapeDefinition2D.h"
 
 
 namespace DF2D::Data
 {
 	struct PhysicsMaterial
 	{
-		b2Shape* shape = new b2PolygonShape();
+		ShapeDefinition2D shape = BoxShapeDefinition2D{};
 
 		float friction = 0.3f;
-	
+
 		float restitution = 0.0f;
-	
+
 		float restitutionThreshold = 1.0f;
-	
+
 		float density = 1.0f;
-	
+
 		bool isSensor = false;
-	
+
 		FilterData filter{};
 	};
 }
