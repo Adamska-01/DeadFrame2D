2c0d37067f50bcae35f89ec1e3a94ef360a78059
Adamska-01 <silverworld@hotmail.it>
2026-08-23 18:01:20 +0200

    Added 'finished' field to 'SpriteAnimationState'

--- DeadFrame2D/include/Data/Components/Animation/SpriteAnimationState.h
+++ DeadFrame2D/include/Data/Components/Animation/SpriteAnimationState.h
@@ -10,6 +10,9 @@ namespace DF2D::Data
 		/** @brief Current frame index (can be fractional for smooth interpolation). */
 		float currentFrame = 0.0f;
 
+		/** @brief Set once a non-looping animation has played through its last frame. */
+		bool finished = false;
+
 		/** @brief Optional flip state. */
 		RenderFlip flipState = RenderFlip::NONE;
 	};
