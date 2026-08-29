811198c7adcc38ca2e66ed8a27fc5ea6ec13a89d
Adamska-01 <silverworld@hotmail.it>
2026-08-29 22:57:58 +0200

    Added some math constants

--- DeadFrame2D/include/Constants/MathConstants.h
+++ DeadFrame2D/include/Constants/MathConstants.h
@@ -10,4 +10,8 @@ namespace DF2D::Constants::MathConstants
 	inline constexpr float PI_f = std::numbers::pi_v<float>;
 
 	inline constexpr float EPSILON = std::numeric_limits<float>::epsilon();
+
+	inline constexpr float RAD_PER_DEG = PI_f / 180.0f;
+
+	inline constexpr float DEG_PER_RAD = 180.0f / PI_f;
 }
