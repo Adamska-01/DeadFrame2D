8071add246e09e55882344dec0ba1791b705380b
Adamska-01 <silverworld@hotmail.it>
2026-09-02 00:59:45 +0200

    Added TODO comment in 'UIContextID'

--- DeadFrame2D/include/Data/Systems/UI/UIContextID.h
+++ DeadFrame2D/include/Data/Systems/UI/UIContextID.h
@@ -2,6 +2,9 @@
 #include <cstdint>
 
 
+// TODO: Instead of using these incremented IDs, why not using uintptr_t? uintptr_t should be unique,
+// thus thread-safe unlike incremented IDs... (this pattern is used in other parts of the codebase,
+// so do some research and evaluate if it is a valid option to use uintptr_t).
 namespace DF2D::Data
 {
 	/** @brief Opaque handle to a UI context (one per Canvas). 0 means invalid. */
