c6d26c205e5c8315a35544b08113c51db3cfde2f
Adamska-01 <silverworld@hotmail.it>
2026-09-08 20:13:56 +0200

    Added 'UINavigationDirection' enum

---
+++ DeadFrame2D/include/Data/Systems/UI/UINavigationDirection.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+namespace DF2D::Data
+{
+	/** 
+	* @brief A direction to move keyboard focus in.
+	*/
+	enum class UINavigationDirection
+	{
+		UP,
+
+		DOWN,
+
+		LEFT,
+
+		RIGHT
+	};
+}
