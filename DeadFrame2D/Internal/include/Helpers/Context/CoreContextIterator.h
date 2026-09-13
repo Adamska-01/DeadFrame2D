f149c0365b571720040620ebcf7d36a30e1c821a
Adamska-01 <silverworld@hotmail.it>
2026-09-13 13:08:02 +0200

    Fixed runtime error when closing the window

--- DeadFrame2D/Internal/include/Helpers/Context/CoreContextIterator.h
+++ DeadFrame2D/Internal/include/Helpers/Context/CoreContextIterator.h
@@ -7,13 +7,14 @@ namespace DF2D::Internal::CoreContextIterator
 	template<typename Func>
 	void ForEach(Data::CoreContext& ctx, Func&& func)
 	{
+		// Order here matters! (destruction order)
 		func(ctx.audioManager);
 		func(ctx.coroutineScheduler);
-		func(ctx.textureManager);
 		func(ctx.input);
 		func(ctx.physicsEngine);
-		func(ctx.renderer);
 		func(ctx.uiManager);
+		func(ctx.textureManager);
+		func(ctx.renderer);
 		func(ctx.window);
 	}
 }
